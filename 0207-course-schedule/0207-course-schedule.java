27aca7dced10a8531def9c3887237f6ae2720bfb
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-06-03 02:38:45 -0400

    Time: 5 ms (67.44%), Space: 45.2 MB (42.08%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        for(int i = 0; i < numCourses; i++){
+            adj.add(new ArrayList<Integer>());
+        }
+
+        for(int i = 0; i < prerequisites.length; i++){
+            int a = prerequisites[i][0];
+            int b = prerequisites[i][1];
+            adj.get(b).add(a);
+        } 
+
+        int[] indegree = new int[numCourses];
+        
+        for(int i = 0; i < adj.size(); i++){
+            for(int j : adj.get(i)){
+                indegree[j]++;
+            }
+        }
+        
+        Queue<Integer> queue = new LinkedList();
+        for(int i = 0; i < indegree.length; i++){
+            if(indegree[i] == 0){
+                queue.add(i);
+            }
+        }
+        ArrayList<Integer> topo = new ArrayList();
+        while(!queue.isEmpty()){
+            int temp = queue.poll();
+            topo.add(temp);
+            numCourses--;
+            for(int i: adj.get(temp)){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    queue.add(i);
+                }
+            }
+        }
+    return numCourses == 0;
+    }
+}
