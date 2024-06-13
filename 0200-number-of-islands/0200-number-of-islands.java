de92fa21b4599588b2fce5334edc942eabf15920
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-06-13 01:01:53 -0400

    Time: 5 ms (26.27%), Space: 51.8 MB (15.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,32 +1,53 @@
-class Solution {
-    private void dfs(int i, int j, char[][] grid){
-        // int cell = grid[i][j];
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length)
-            return;
-        if(grid[i][j] == '0')
-            return;
-        
-        grid[i][j] = '0';
-
-        dfs(i, j + 1, grid);
-        dfs(i + 1, j, grid);
-        dfs(i, j - 1, grid);
-        dfs(i - 1, j, grid);
-
+class Pair{
+    int first;
+    int second;
+    public Pair(int first, int second){
+        this.first = first;
+        this.second = second;
     }
+}
+class Solution {
+
+    private void bfs(char[][] grid, int row, int col, boolean[][] visited, int n, int m){
+        Queue<Pair> queue = new LinkedList<>();
+
+        queue.add(new Pair(row, col));
+        visited[row][col] = true;
+
+        int[] drow = {0,1,0,-1};
+        int[] dcol = {1,0,-1,0};
+
+        while(!queue.isEmpty()){
+            Pair p = queue.poll();
+            int first = p.first;
+            int second = p.second;
+
+            for(int i = 0; i < 4; i++){
+                int nrow = first + drow[i];
+                int ncol = second + dcol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
+                    visited[nrow][ncol] = true;
+                    queue.add(new Pair(nrow, ncol));
+                }
+            }
+
+        }
+    }
+    
     public int numIslands(char[][] grid) {
-        int count = 0;
         int n = grid.length;
         int m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
+        int count = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(grid[i][j] == '1'){ 
-                dfs(i, j, grid);
-                count++;
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    bfs(grid, i, j, visited, n, m);
+                    count++;
                 }
             }
         }
-    return count;
+        return count;
     }
-    
 }
