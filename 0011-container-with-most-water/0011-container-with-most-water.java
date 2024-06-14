71cd987fe8f95581b1e77cbb7b8315f758033585
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-06-14 19:51:05 -0400

    Time: 4 ms (87.32%), Space: 56.8 MB (95.53%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,5 +1,8 @@
 class Solution {
     public int maxArea(int[] height) {
+        if (height == null || height.length < 2) {
+            return 0;
+        }
         int maxArea = 0;
         int l = 0;
         int r = height.length - 1;
