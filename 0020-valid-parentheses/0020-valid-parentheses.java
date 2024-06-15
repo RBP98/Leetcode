e4f7258b2ce59724b7b2c1906fd7530771f38bde
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-06-14 20:14:46 -0400

    Time: 2 ms (54.81%), Space: 41.2 MB (71.51%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,16 +1,17 @@
-public class Solution {
-    public static boolean isValid(String s) {
-        while (true) {
-            if (s.contains("()")) {
-                s = s.replace("()", "");
-            } else if (s.contains("{}")) {
-                s = s.replace("{}", "");
-            } else if (s.contains("[]")) {
-                s = s.replace("[]", "");
-            } else {
-                // If the string becomes empty, it indicates all brackets are matched.
-                return s.isEmpty();
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0 ; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push(')');
+            }else if(s.charAt(i) == '{'){
+                stack.push('}');
+            }else if(s.charAt(i) == '['){
+                stack.push(']');
+            }else if(stack.isEmpty() || stack.pop() !=  s.charAt(i)){
+                return false;
             }
         }
+        return stack.isEmpty();
     }
 }
