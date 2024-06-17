7e5a05ae76aa8181f455f37ae4f08f8cefe42fd2
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-06-17 12:58:28 -0400

    Time: 68 ms (5.03%), Space: 44.9 MB (5.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,15 @@
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(char c: s.toCharArray()){
-            if(c == '(' || c == '{' || c == '[') stack.push(c);
-            else {
-                if(stack.isEmpty()){
-                    return false;
-                }
-                if(c == ')' && stack.peek() == '(' 
-                || c == '}' && stack.peek() == '{'
-                || c == ']' && stack.peek() == '['){
-                    stack.pop();
-                }else{
-                    return false;
-                }
-            } 
+public class Solution {
+    public static boolean isValid(String s) {
+        while (true) {
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            } else if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            } else if (s.contains("[]")) {
+                s = s.replace("[]", "");
+            } else {
+                return s.isEmpty();
+            }
         }
-        return stack.isEmpty();
     }
 }
