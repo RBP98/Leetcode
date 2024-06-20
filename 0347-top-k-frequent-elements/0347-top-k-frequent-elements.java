e7bc353beb4147eb232ce8700cd4ad3d7b2d7bc7
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-06-20 18:22:13 -0400

    Time: 11 ms (90.42%), Space: 45.2 MB (95.87%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -5,14 +5,35 @@ class Solution {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (map.get(b)) - (map.get(a)));
-        for(int i: map.keySet())
-            pq.add(i);
-
-        int res[] = new int[k];
-        for(int i =0 ; i < k; i++){
-            res[i] = pq.poll();
+         List<Integer>[] bucket = new List[nums.length + 1];
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int frequency = entry.getValue();
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(entry.getKey());
         }
+
+        List<Integer> resultList = new ArrayList<>();
+        for (int i = nums.length; i >= 0 && resultList.size() < k; i--) {
+            if (bucket[i] != null) {
+                resultList.addAll(bucket[i]);
+            }
+        }
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = resultList.get(i);
+        }
+
+        // PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (map.get(b)) - (map.get(a)));
+        // for(int i: map.keySet())
+        //     pq.add(i);
+
+        // int res[] = new int[k];
+        // for(int i =0 ; i < k; i++){
+        //     res[i] = pq.poll();
+        // }
 
     return res;
     }
