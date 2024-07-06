70999597338c1206cbddb0af5103af63ed5bb46b
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-07-05 22:24:40 -0400

    Time: 25 ms (54.41%), Space: 44.7 MB (90.21%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -0,0 +1,37 @@
+class Solution {
+    private int findMax(int[] piles){
+        int max = Integer.MIN_VALUE;
+        for(int i: piles){
+            if(max < i){
+                max = i;
+            }
+        }
+        return max;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = findMax(piles);
+        int low = 0; 
+        int high = max;
+        int answer = Integer.MAX_VALUE;
+        while(low <= high){
+            int mid = (low + high)/2;
+            int hours = calculateTotalHours(piles, mid);
+            if(hours <= h){
+                answer = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return answer;
+    }
+
+    private int calculateTotalHours(int[] piles, int bph){
+        int totalHours = 0;
+        for(int i = 0; i < piles.length; i++){
+            totalHours += Math.ceil((double)piles[i]/(double)bph);
+        }
+        return totalHours;
+    }
+    
+}
