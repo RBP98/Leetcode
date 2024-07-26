d8f91d28adac444b97777dd748e24942b565def7
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-07-26 00:59:00 -0400

    Time: 6 ms (24.56%), Space: 52.3 MB (9.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -6,43 +6,46 @@ class Pair{
         this.second = second;
     }
 }
-
 class Solution {
-    private void bfs(int currRow, int currCol, char[][] grid, boolean[][] visited, int rows, int cols){
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(currRow,currCol));
-        visited[currRow][currCol] = true;
+    private void bfs(int row, int col, boolean[][] visited, char[][] grid, int m, int n){
+        Queue<Pair> queue = new LinkedList<Pair>();
+        queue.add(new Pair(row, col));
+        visited[row][col] = true;
         while(!queue.isEmpty()){
             Pair p = queue.poll();
             int first = p.first;
-            int second  = p.second;
-            int[] drow = {0, 1, 0, -1};
-            int[] dcol = {1, 0, -1, 0};
-            for(int i = 0; i < 4; i++){
-                int nrow = first + drow[i];
-                int ncol = second + dcol[i];
+            int second = p.second;
 
-                if(nrow >= 0 && nrow < rows && ncol >= 0 && ncol < cols && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
-                    queue.add(new Pair(nrow, ncol));
+            int drow[] = {0, 1, 0, -1};
+            int dcol[] = {1, 0, -1, 0};
+
+            for(int i = 0; i < 4; i++){
+                int nrow = drow[i] + first;
+                int ncol = dcol[i] + second;
+
+                if(nrow >=0 && nrow < m && ncol >= 0 && ncol < n && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
                     visited[nrow][ncol] = true;
+                    queue.add(new Pair(nrow, ncol));
                 }
             }
+
+            
         }
     }
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        Queue<Pair> queue = new LinkedList<>();
         boolean[][] visited = new boolean[m][n];
         int count = 0;
+        
         for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = 0 ; j < n; j++){
                 if(!visited[i][j] && grid[i][j] == '1'){
-                    bfs(i, j, grid, visited, m, n);
+                    bfs(i, j, visited, grid, m, n);
                     count++;
                 }
             }
         }
-    return count;
+        return count;
     }
 }
