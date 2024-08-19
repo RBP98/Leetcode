f4ff1f164574f6ebcc262f18dc0f5ddca0489d88
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-08-19 10:52:51 -0400

    Time: 1 ms (80.12%), Space: 45.5 MB (66.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,36 +1,36 @@
 class Solution {
-    public void markCol(int col, int[][] matrix){
+    private void markRow(int[][] matrix, int row){
+        for(int i = 0; i < matrix[row].length; i++){
+            matrix[row][i] = 0;
+        }
+    }
+    private void markCol(int[][] matrix, int col){
         for(int i = 0; i < matrix.length; i++){
             matrix[i][col] = 0;
         }
     }
-    public void markRow(int row, int[][] matrix){
-        for(int i = 0; i < matrix[0].length; i++){
-            matrix[row][i] = 0;
-        }
-    }
     public void setZeroes(int[][] matrix) {
-        int[] rowArr = new int[matrix.length];
-        int[] colArr = new int[matrix[0].length];
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
                 if(matrix[i][j] == 0){
-                    colArr[j] = -1;
-                    rowArr[i] = -1;
+                    row[i] = -1;
+                    col[j] = -1;
                 }
             }
         }
-
-        for(int i = 0; i < matrix.length; i++){
-            if(rowArr[i] == -1){
-                markRow(i, matrix);
+        for(int i = 0;i < row.length; i++){
+            if(row[i] == -1){
+                markRow(matrix, i);
             }
         }
 
-        for(int i = 0; i < matrix[0].length; i++){
-            if(colArr[i] == -1){
-                markCol(i, matrix);
+        for(int i = 0;i < col.length; i++){
+            if(col[i] == -1){
+                markCol(matrix, i);
             }
         }
+
     }
 }
