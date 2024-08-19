730c8e3dca611b615dc601f3080daf332b6d2af3
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-08-19 14:18:51 -0400

    Time: 0 ms (100%), Space: 41.1 MB (45.34%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,16 +1,29 @@
 class Solution {
-    private long nCr(int n, int r){
+    //BruteForce Approach
+      
+    /*private long nCr(int n, int r){
         long res = 1;
         for(long i = 0; i < r; i++){
             res = res * (n-i);
             res = res / (i+1) ;
         }
         return res;
-    }
+    }*/
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> result = new ArrayList<Integer>();
+      //BruteForce Approach
+      /*  List<Integer> result = new ArrayList<Integer>();
         for(int i = 0; i <= rowIndex; i++){
             result.add((int)nCr(rowIndex, i));
+        }
+        return result;
+        */
+
+        List<Integer> result = new ArrayList<>();
+        result.add(1);
+        long temp = 1;
+        for(int i = 0; i < rowIndex; i++){
+            temp = temp * (rowIndex-i)/ (i + 1);
+            result.add((int)temp);
         }
         return result;
     }
