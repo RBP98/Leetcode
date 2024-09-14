3d0c9f14d592d0a0c86acd5b3d9493997930c857
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-09-14 17:04:47 -0400

    Time: 2 ms (98.51%), Space: 44.8 MB (64.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             int rem = target - nums[i];
             if(map.containsKey(rem)){
-                 return new int[]{i, map.get(rem)};
-             }
-             map.put(nums[i],i);
+                return new int[]{map.get(rem), i};
+            }
+            map.put(nums[i], i);
         }
         return new int[]{};
     }
