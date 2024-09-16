f50638670a9d578831687fd5008f196e8744cdfd
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-09-16 19:50:24 -0400

    Time: 7 ms (39.76%), Space: 44.5 MB (68.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,20 +1,47 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashSet<Character> hs = new HashSet<>();
         int left = 0;
         int right = 0;
-        int currentLongest = 0;
-        while(right < s.length()){
-            if(!hs.contains(s.charAt(right))){
-                hs.add(s.charAt(right));
+        int n = s.length();
+        int max = 0;
+        // Set<Character> set = new HashSet<>();
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        while(right < n){
+            map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
+            if(map.size() == right - left + 1){
+                max = Math.max(max, right - left + 1);
                 right++;
-                currentLongest = Math.max(hs.size(), currentLongest);
-            }else{
-                hs.remove(s.charAt(left));
-                left++;
+            }else if(map.size() < right - left + 1){
+                while(map.size() < right - left + 1){
+                    map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
+                    if(map.get(s.charAt(left)) == 0){
+                        map.remove(s.charAt(left));
+                    }
+                    left++;
+                }
+                right++;
             }
-        }      
-    return currentLongest;
-    }
+            
 
+
+
+
+
+            // if(!set.contains(s.charAt(right))){
+            //     set.add(s.charAt(right));
+            //     max = Math.max(max, right - left + 1);
+            // }else{
+            //     while(s.charAt(left) != s.charAt(right)){
+            //         set.remove(s.charAt(left));
+            //         left++;
+            //     }
+                
+            //     max = Math.max(max, right - left + 1);
+            //     right++;
+            //     left++;
+            // }
+
+        }
+        return max;
+    }
 }
