afb7d72c376a3f6b616d824ac3add65d6dbe02fb
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-11-06 13:38:10 -0500

    Time: 10 ms (14.29%), Space: 44.5 MB (25.16%) - LeetHub

---
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
@@ -0,0 +1,27 @@
+class Solution {
+    private int countSetBits(int num){
+        int count = 0;;
+        while(num != 0){
+            if(num%2 == 1) count++;
+            num = num/2;
+        }
+        return count;
+    }
+
+    public boolean canSortArray(int[] nums) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i; j < nums.length; j++){
+                if(nums[j] < nums[i]){
+                    if(countSetBits(nums[j]) == countSetBits(nums[i])){
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
