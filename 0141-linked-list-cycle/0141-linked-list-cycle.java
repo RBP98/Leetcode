220ffc0cde68d6bd52e70a78b5adc9c00bb3a607
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-11-08 13:19:19 -0500

    Time: 0 ms (100%), Space: 45 MB (7.5%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -15,11 +15,13 @@ public class Solution {
         ListNode slow = head;
 
         while(fast != null && fast.next != null){
-            fast = fast.next.next;
-            slow = slow.next;
-            if(fast == slow) return true;
-        }    
-        return false;
-    }
+                fast = fast.next.next;
+                slow = slow.next;
 
+                if(slow == fast) return true;
+        }
+    
+            
+     return false;   
+    }
 }
