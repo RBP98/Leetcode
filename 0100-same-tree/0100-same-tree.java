dd0114da1e2f6b8e7e0c92bf8e7a638d4efd15fa
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-11-29 22:20:22 -0500

    Time: 0 ms (100%), Space: 40.4 MB (98.49%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,7 +15,17 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null || q == null) return p == q;
-        return p.val == q.val && isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+
+        if(p.val != q.val){
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
