541a35a04fe9d1777bc4e70e68251be59c71e913
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-12-03 01:38:20 -0500

    Time: 1114 ms (17.32%), Space: 66 MB (54.92%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -6,9 +6,10 @@ class Solution {
         }
         int max = 0;
         for(int i = 0; i < nums.length; i++){
-            int count = 0;
-            int curr = nums[i];
             if(!set.contains(nums[i] - 1)){
+                int count = 0;
+                int curr = nums[i];
+            
                 while(set.contains(curr++)){
                     count++;
                 }
