2f5f08f58ad65dfac368ec3d002de3de6f0b166c
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-12-20 09:44:38 -0500

    Time: 48 ms (59.45%), Space: 73.9 MB (23.65%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i = 1; i < intervals.length; i++){
             if(intervals[i][0] >= end){
                 count++;
-                end  = Math.max(intervals[i][1], end); 
+                end  = intervals[i][1];
             }
         }
         return intervals.length - count - 1;
