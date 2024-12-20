7be41be48193f6540c30306518bb472ad7c089e3
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-12-20 16:44:49 -0500

    Time: 14 ms (31.07%), Space: 44.8 MB (19.1%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] arr = new int[26];
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            arr[c - 'a']++;
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()){
+           map.put(c, map.getOrDefault(c, 0) + 1); 
         }
 
-        for(int i = 0; i < t.length(); i++){
-            char c = t.charAt(i);
-            arr[c - 'a']--;
+        for(char c: t.toCharArray()){
+            if(!map.containsKey(c)){
+                return false;
+            }else{
+                map.put(c, map.get(c) - 1);
+            }
         }
 
-        for(int i: arr){
-            if(i != 0) return false;
+        for(int i: map.values()){
+            if(i != 0){
+                return false;
+            }
         }
-
         return true;
     }
 }
