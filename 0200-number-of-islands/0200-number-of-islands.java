7eaf1428bb63e2ca56adbd9488c16170487cdb1a
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-12-21 09:16:14 -0500

    Time: 18 ms (7.02%), Space: 52.8 MB (6.68%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,14 +1,23 @@
+class Pair{
+    int first;
+    int second;
+
+    public Pair(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+}
+
 class Solution {
-    private void bfs(char[][] grid, boolean[][] visited, int row, int col, int m, int n){
-        Queue<Pair> queue = new LinkedList<Pair>();
+    private void bfs(int row, int col, boolean[][] visited, char[][] grid, int m, int n){
+        Queue<Pair> queue = new LinkedList<>();
         queue.add(new Pair(row, col));
         visited[row][col] = true;
-        
+
         while(!queue.isEmpty()){
             Pair p = queue.poll();
             int first = p.first;
             int second = p.second;
-
             int[] drow = {0, 1, 0, -1};
             int[] dcol = {1, 0, -1, 0};
 
@@ -17,36 +26,29 @@ class Solution {
                 int ncol = second + dcol[i];
 
                 if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
-                    visited[nrow][ncol] = true;
                     queue.add(new Pair(nrow, ncol));
+                    visited[nrow][ncol] = true;
+                    
                 }
-            }
-        } 
-    }
 
+            }
+        }
+    }
 
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         boolean[][] visited = new boolean[m][n];
-        int count  = 0;
-        for(int i =0; i < m; i++){
+        int count = 0;
+        for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(!visited[i][j] && grid[i][j] == '1'){
-                    bfs(grid, visited, i, j, m, n);
+                    bfs(i , j, visited, grid, m, n);
+                    System.out.println();
                     count++;
                 }
             }
         }
-        return count;
+    return count;
     }
 }
-
-class Pair{
-        int first;
-        int second;
-        public Pair(int first, int second){
-            this.first = first;
-            this.second = second;
-        }
-    }
