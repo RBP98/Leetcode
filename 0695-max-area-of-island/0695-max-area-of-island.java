00b108f46badb7ba3cb23305aaf6d458a282689a
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-12-21 09:54:08 -0500

    Time: 4 ms (14.67%), Space: 45.3 MB (9.67%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,6 +1,7 @@
 class Pair{
     int first;
     int second;
+    
     public Pair(int first, int second){
         this.first = first;
         this.second = second;
@@ -8,45 +9,47 @@ class Pair{
 }
 class Solution {
 
-    private int bfs(int row, int col, int m, int n, boolean[][] visited, int[][] grid){
+    private int bfs(int row, int col, boolean[][] visited,int[][] grid, int m, int n){
         Queue<Pair> queue = new LinkedList<>();
-        int area = 1;
         queue.add(new Pair(row, col));
         visited[row][col] = true;
-
+        int area = 1;
         while(!queue.isEmpty()){
             Pair p = queue.poll();
+            int first = p.first;
+            int second = p.second;
 
-            int drow[] = {0, 1, 0, -1};
-            int dcol[] = {1, 0, -1, 0};
+            int[] drow = {0, 1, 0, -1};
+            int[] dcol = {1, 0, -1, 0};
 
             for(int i = 0; i < 4; i++){
-                int nrow = p.first + drow[i];
-                int ncol = p.second + dcol[i];
+                int nrow = first + drow[i];
+                int ncol = second + dcol[i];
 
-                if(nrow >= 0 && ncol >=0 && nrow < m && ncol < n && !visited[nrow][ncol] && grid[nrow][ncol] == 1){
-                    queue.add(new Pair(nrow, ncol));
+                if(nrow >= 0 && nrow < m && ncol >=0 && ncol < n && !visited[nrow][ncol] && grid[nrow][ncol] == 1){
                     visited[nrow][ncol] = true;
+                    queue.add(new Pair(nrow, ncol));
                     area++;
                 }
             }
         }
         return area;
     }
-
     public int maxAreaOfIsland(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int maxArea = 0;
         boolean[][] visited = new boolean[m][n];
+        int maxArea = 0;
+
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(!visited[i][j] && grid[i][j] == 1){
-                    int area = bfs(i, j, m, n, visited, grid);
-                    maxArea = Math.max(maxArea, area);
+                if(grid[i][j] == 1 && !visited[i][j]){
+                    int currentArea = bfs(i, j, visited, grid, m, n); 
+                    maxArea = Math.max(maxArea, currentArea);
                 }
             }
         }
-        return maxArea;
+
+    return maxArea;
     }
 }
