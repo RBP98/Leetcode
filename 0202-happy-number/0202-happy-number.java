4b9330e89a958757749b94cbca676e9c403bde8b
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-12-21 12:32:48 -0500

    Time: 13 ms (5.3%), Space: 41.7 MB (5.86%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+         
+        while(n != 1){
+            int sum = 0;
+            while(n != 0){
+                int temp = n % 10;
+                sum = sum + (temp * temp);
+                n = n/10;
+            }
+            n = sum;
+            
+            if(!set.contains(sum)){
+                set.add(sum);
+                System.out.println(sum);
+            }else{
+                return false;
+            }
+
+            
+            }
+
+         return true;
+            
+        
+    }
+}
