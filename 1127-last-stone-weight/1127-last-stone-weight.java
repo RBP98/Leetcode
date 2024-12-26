3a7b771989da57a68b0c45ae9518b506840a79cf
RBP98 <47711591+RBP98@users.noreply.github.com>
2024-12-25 20:53:54 -0500

    Time: 1 ms (98.77%), Space: 41.3 MB (34.73%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.java
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -2,16 +2,14 @@ class Solution {
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
         for(int i: stones) maxHeap.offer(i);
-        for(int i = 0; i < stones.length; i++){
-            if(maxHeap.size() == 1){
-                return maxHeap.poll();
-            }
-            int diff = maxHeap.poll() - maxHeap.poll();
-            if(diff > 0){
+        while(maxHeap.size() > 1) {
+            int w1 = maxHeap.poll();
+            int w2 = maxHeap.poll();
+            int diff = w1-w2;
+            if(diff != 0) {
                 maxHeap.offer(diff);
             }
-            if(maxHeap.size() == 0) return 0;
         }
-    return 0;
+        return maxHeap.size() == 0 ? 0 : maxHeap.poll();
     }
 }
