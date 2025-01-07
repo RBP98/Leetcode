f361796c5a6188324845cd59efd9ae5642fc4898
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-01-06 21:45:13 -0500

    Time: 128 ms (6.83%), Space: 42.4 MB (37.89%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -1,21 +1,17 @@
 class Solution {
-    private boolean subInMain(String sub, String main){
-        return main.contains(sub);
-    }
-
     public List<String> stringMatching(String[] words) {
         Set<String> output = new HashSet<>();
         
         for(int i = 0; i < words.length; i++){
             for(int j = 0; j < words.length; j++){
                 if(i != j){
-                    if(words[i].length() < words[j].length()){
-                        if(subInMain(words[i], words[j])){
-                            output.add(words[i]);
+                    if(words[i].length() > words[j].length()){
+                        if(words[i].contains(words[j])){
+                            output.add(words[j]);
                         }
                     }else{
-                        if(subInMain(words[j], words[i])){
-                            output.add(words[j]);
+                        if(words[j].contains(words[i])){
+                            output.add(words[i]);
                         }
                     }
                     
