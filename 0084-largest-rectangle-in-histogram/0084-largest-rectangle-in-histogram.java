fd733b317fa45ec04fae6b62a4846b6c9a9de224
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-01-30 16:36:15 -0500

    Time: 2192 ms (5.03%), Space: 56.3 MB (96.8%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,71 @@
+class Solution {
+    private int[] nearestSmallestLeft(int[] arr){
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] output = new int[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            if(stack.isEmpty()){
+                output[i] = -1;
+            }else if(arr[i] > arr[stack.peek()]){
+                output[i] = stack.peek();
+            }else if(arr[i] <= arr[stack.peek()]){
+                while(!stack.isEmpty() && arr[i] <= arr[stack.peek()]){
+                    stack.pop();
+                }
+                if(stack.isEmpty()){
+                    output[i] = -1;
+                }else{
+                    output[i] = stack.peek();
+                }
+            }
+            stack.push(i);
+        }
+        return output;
+    }
+
+    private int[] nearestSmallestRight(int[] arr){
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] output = new int[arr.length];
+        for(int i = arr.length - 1; i >= 0; i--){
+            if(stack.isEmpty()){
+                output[i] = arr.length;
+            }else if(arr[i] <= arr[stack.peek()]){
+                while(!stack.isEmpty() && arr[i] <= arr[stack.peek()]){
+                    stack.pop();
+                }
+                if(stack.isEmpty()){
+                    output[i] = arr.length;
+                }else{
+                    output[i] = stack.peek();
+                }
+            }else if(arr[i] > arr[stack.peek()]){
+                output[i] = stack.peek();
+            }
+            stack.push(i);
+        }
+        return output;
+    }
+    
+    public int largestRectangleArea(int[] heights) {
+        int[] width = new int[heights.length];
+        int[] nsr = nearestSmallestRight(heights);
+        int[] nsl = nearestSmallestLeft(heights);
+
+        int maxArea = 0;
+
+        for(int i: nsr){
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        for(int i: nsl){
+            System.out.print(i + " ");
+        }
+        for(int i = 0; i < width.length; i++){
+            width[i] = nsr[i] - nsl[i] - 1;
+
+            maxArea = Math.max(maxArea, width[i] * heights[i]);
+        }
+
+    return maxArea;
+    }
+}
