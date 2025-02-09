61c9e3ccfd7cdb5adc1e67d276786f6ccf319bcc
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-09 08:57:32 -0500

    Time: 21 ms (29.01%), Space: 45.6 MB (54.1%) - LeetHub

---
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -0,0 +1,71 @@
+class Pair {
+    int first;
+    int second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+class Solution {
+    private int bfs(int[][] grid, boolean[][] visited, int m, int n) {
+        Queue<Pair> queue = new LinkedList<>();
+        int currDistance = 1;
+        queue.add(new Pair(0, 0));
+
+        visited[0][0] = true;
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+
+            for (int j = 0; j < size; j++) {
+                Pair cell = queue.poll();
+
+                int row = cell.first;
+                int col = cell.second;
+
+                int[][] directions = new int[][] { { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 }, { -1, -1 }, { -1, 0 }, { -1, 1 } };
+
+                if (row == m - 1 && col == n - 1)
+                    return currDistance;
+
+                for (int i = 0; i < 8; i++) {
+                    int newRow = row + directions[i][0];
+                    int newCol = col + directions[i][1];
+
+                    if (newRow >= 0 && newCol >= 0 && newRow < m && newCol < n && grid[newRow][newCol] == 0
+                            && !visited[newRow][newCol]) {
+                        queue.add(new Pair(newRow, newCol));
+                        visited[newRow][newCol] = true;
+
+                    }
+                }
+
+            }
+
+            currDistance++;
+
+        }
+        // if(visited[m - 1][n - 1] == false){
+        // currDistance = -1;
+        // }
+
+        return -1;
+    }
+
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+
+        if (grid[0][0] != 0 || grid[m - 1][n - 1] != 0) {
+
+            return -1;
+        }
+
+        return bfs(grid, visited, m, n);
+    }
+
+}
