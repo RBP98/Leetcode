9e670ec4972b7d4db70e2998684ed9628442b5e3
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-09 14:25:43 -0500

    Time: 0 ms (100%), Space: 40.6 MB (36.23%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int getSum(int a, int b) {
+        int x = Math.abs(a);
+        int y = Math.abs(b);
+
+        int answer = 0;
+        int sign = a > 0 ? 1 : -1;
+
+        if(x < y) return getSum(b, a);
+
+        if(a*b >= 0){
+            while(y != 0){
+                answer = x ^ y;
+                int carry = (x & y) << 1;
+                x = answer;
+                y = carry;
+            }
+        }else{
+            while(y != 0){
+                answer = x ^ y;
+                int borrow = ((~x) & y) << 1;
+                x = answer;
+                y = borrow;
+            }
+
+        }
+
+        return x * sign;
+    }
+}
