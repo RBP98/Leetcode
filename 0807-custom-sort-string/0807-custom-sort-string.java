80b0d5c8348901008f60dc766a8194a8dbbbd35c
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-09 14:56:03 -0500

    Time: 0 ms (100%), Space: 41.5 MB (87.24%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.java
+++ 0807-custom-sort-string/0807-custom-sort-string.java
@@ -1,28 +1,29 @@
 class Solution {
     public String customSortString(String order, String s) {
-        Map<Character, Integer> map = new HashMap<>();
+        int[] freqMap = new int[26];
+
         for(char c: s.toCharArray()){
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            freqMap[c - 'a']++;
         }
         StringBuilder sb = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
         for(int i = 0; i < order.length(); i++){
-            if(map.containsKey(order.charAt(i))){
-                int count = map.get(order.charAt(i));
-                for(int j = 0; j < count; j++){
-                    sb.append(order.charAt(i));
-                    map.put(order.charAt(i), map.get(order.charAt(i)) - 1);
-                }    
-            }
-        }
-
-        for(Map.Entry<Character, Integer> entry: map.entrySet()){
-            if(entry.getValue() != 0){
-                for(int i = 0; i < entry.getValue(); i++){
-                    sb.append(entry.getKey());
+            char current = order.charAt(i); 
+            if(freqMap[current - 'a'] != 0){
+                int currentCount = freqMap[current - 'a'];
+                for(int j = 0; j < currentCount; j++){
+                    sb.append(current);
+                    freqMap[current - 'a']--;
                 }
             }
         }
-    return sb.toString();
+
+        for(int i = 0; i < 26; i++){
+            while(freqMap[i] != 0){
+                sb.append((char)(i + 'a'));
+                freqMap[i]--;
+            }
+        }
+
+        return sb.toString();
     }
 }
