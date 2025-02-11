99f4710364516101e0ca9c63ce5d8d2154dd6411
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-11 17:24:38 -0500

    Time: 2 ms (46.45%), Space: 47.8 MB (56.96%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,58 @@
+class DisjointSet{
+    List<Integer> parent;
+    List<Integer> size;
+
+    public DisjointSet(int n){
+        parent = new ArrayList<>();
+        size = new ArrayList<>();
+        for(int i = 0; i < n+1; i++){
+            parent.add(i);
+            size.add(1);
+        }       
+    }
+
+    public int findParent(int node){
+        if(node == parent.get(node)){
+            return node;
+        }
+        int grandParent = findParent(parent.get(node));
+        parent.set(node, grandParent);
+        return grandParent;
+    }
+
+    public void union(int a, int b){
+        int parentOfA = findParent(a);
+        int parentOfB = findParent(b);
+
+        if(parentOfA == parentOfB) return;
+        if(size.get(parentOfA) < size.get(parentOfB)){
+            parent.set(parentOfA, parentOfB);
+            size.set(parentOfB, size.get(parentOfA) + size.get(parentOfB));
+        }else{
+            parent.set(parentOfB, parentOfA);
+            size.set(parentOfA, size.get(parentOfA) + size.get(parentOfB));
+        }
+        
+    }
+}
+
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        DisjointSet ds = new DisjointSet(n);
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(isConnected[i][j] == 1){
+                    ds.union(i + 1, j + 1);
+                }
+            }
+        }
+        int count = 0;
+        for(int i = 1; i < ds.parent.size(); i++){
+            if(ds.parent.get(i) == i){
+                count++;
+            }
+        }
+        return count;
+    }
+}
