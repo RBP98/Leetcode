64af8e159a06a51a469abd74c32ce5e54271d962
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-13 19:02:42 -0500

    Time: 5 ms (57.58%), Space: 45.1 MB (24.49%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,34 +1,29 @@
 class Solution {
+    
     public int romanToInt(String s) {
-    Map<String, Integer> map = new HashMap<>();
-    map.put("I", 1);
-    map.put("V", 5);
-    map.put("X", 10);
-    map.put("L", 50);
-    map.put("C", 100);
-    map.put("D", 500);
-    map.put("M", 1000);
-    map.put("IV", 4);
-    map.put("IX", 9);
-    map.put("XL", 40);
-    map.put("XC", 90);
-    map.put("CD", 400);
-    map.put("CM", 900);    
-    int i = 0;
-    int sum = 0;
-    while(i < s.length()){
-        if(i < s.length() - 1){
-        String twoChar = s.substring(i, i + 2);
-        if(map.containsKey(twoChar)){
-            sum = sum + map.get(twoChar);
-            i = i + 2;
-            continue;
-        }
-        }
-        sum = sum + map.get(s.substring(i, i + 1));
-        i++;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int n = s.length();
         
-    }
-    return sum;
+        char next = s.charAt(n - 1);
+        int total = map.get(next);
+
+        for(int i = n - 2; i >= 0; i--){
+            char curr = s.charAt(i);
+            if(map.get(curr) < map.get(next)){
+                total = total - map.get(curr);
+            }else{
+                total = total + map.get(curr);
+            }
+            next = curr;
+        }  
+        return total;  
     }
 }
