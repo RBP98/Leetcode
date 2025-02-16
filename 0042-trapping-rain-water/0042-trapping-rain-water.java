878b405cd4113d6f458eb00bb4852bd4c666268a
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-16 15:56:43 -0500

    Time: 1 ms (64.31%), Space: 45.8 MB (92.44%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,20 +1,22 @@
 class Solution {
     public int trap(int[] height) {
-        int[] preMax = new int[height.length];
-        int[] sufMax = new int[height.length];
-        preMax[0] = height[0];
-        for(int i = 1;i < height.length; i++){
-            preMax[i] = Math.max(preMax[i - 1], height[i]);
+        int n = height.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        leftMax[0] = height[0];
+        rightMax[n - 1] = height[n - 1];
+        int ans = 0;
+        for(int i = 1; i < n; i++){
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
         }
-        sufMax[height.length - 1] = height[height.length - 1];
-       
-        for(int i = height.length - 2; i >= 0; i--){
-            sufMax[i] = Math.max(sufMax[i + 1], height[i]);
+        for(int i = n - 2; i >= 0; i--){
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
         }
-        int sum = 0;
-        for(int i = 0; i < height.length; i++) {
-            sum = sum + Math.max(0, (Math.min(preMax[i], sufMax[i]) - height[i]));
-        }        
-        return sum;
+        
+
+        for(int i = 0; i < n; i++){
+            ans = ans + (Math.min(rightMax[i],leftMax[i]) - height[i]);
+        }
+        return ans;
     }
 }
