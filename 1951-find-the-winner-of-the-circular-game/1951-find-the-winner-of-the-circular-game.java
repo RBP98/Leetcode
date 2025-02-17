d063a4a5248f6e355ba6bde641a2aba916956390
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-16 20:22:16 -0500

    Time: 43 ms (21.86%), Space: 44.2 MB (21.35%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        Queue<Integer> circle = new ArrayDeque<>();
+        Queue<Integer> circle = new LinkedList<>();
         for(int i = 1; i <= n; i++){
             circle.add(i);
         }
