ac477c424962dbd75dde5aa803c43a19ca5cc4d6
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-18 21:10:34 -0500

    Time: 8 ms (87.19%), Space: 46.5 MB (70.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        if(intervals.length <= 1) return intervals;
+        
         Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
         List<int[]> output= new ArrayList<>(); 
 
