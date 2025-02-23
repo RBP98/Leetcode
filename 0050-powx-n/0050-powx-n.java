50b8cd81d6781482e4db9c670eecf8e7eb61cee8
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-23 18:08:56 -0500

    Time: 0 ms (100%), Space: 41.8 MB (87.26%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -7,13 +7,8 @@ class Solution {
         }
         return helper(x, N);
     }
-    double ans = 1;
     private double helper(double x, long n){
-        // if(x == 0 && n == 1) return 0;
-        if(n == 0) return 1;
-        // if(x == 1 || n == 1){
-        //     return x;
-        // }
+       if(n == 0) return 1;
         if(n%2 == 1){
             return x * helper(x * x, n / 2);
         }else{
