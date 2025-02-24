5b558dd8b057c99ef75c28390a1244287d3bc1c5
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-24 16:20:09 -0500

    Time: 4 ms (41.95%), Space: 42.6 MB (65.98%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -34,19 +34,24 @@ class Solution {
             int level = t.level;
             int vertical = t.vertical;
             TreeNode node = t.node;
-
-            PriorityQueue<TreeNode> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.val, b.val));
-            Map<Integer, PriorityQueue<TreeNode>> tempMap = new TreeMap<>();
+            Map<Integer, PriorityQueue<TreeNode>> tempMap;    
+            PriorityQueue<TreeNode> pq; 
+            
             if(map.containsKey(vertical)){
                 tempMap = map.get(vertical); 
+            }else{
+                tempMap = new TreeMap<>();
+                map.put(vertical, tempMap);
             }
+
             if(tempMap.containsKey(level)){
                 pq = tempMap.get(level);
+            }else{
+                pq = new PriorityQueue<>((a,b) -> Integer.compare(a.val, b.val));
+                tempMap.put(level, pq);
             }
 
             pq.add(node);
-            tempMap.put(level, pq);
-            map.put(vertical, tempMap);
             if(node.left != null){
                 queue.add(new Tuple(level + 1, vertical - 1, node.left));
             }
