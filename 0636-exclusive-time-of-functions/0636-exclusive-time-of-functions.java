c2eca090d164bfb5d6c0d8506599652d204e6905
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-25 13:23:17 -0500

    Time: 13 ms (23.83%), Space: 45.4 MB (64.37%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+
+        Stack<Integer> stack = new Stack<>();
+        String[] first = logs.get(0).split(":");
+        int prev = Integer.parseInt(first[2]);
+        stack.push(Integer.parseInt(first[0]));
+        int[] res = new int[n];
+        for(int i = 1; i < logs.size(); i++){
+            String[] s = logs.get(i).split(":");
+            int currFunc = Integer.parseInt(s[0]);
+            int currTime = Integer.parseInt(s[2]);
+            if(s[1].equals("start")){
+                if(!stack.isEmpty()){
+                    res[stack.peek()] = res[stack.peek()] + (currTime - prev);
+                }
+                stack.push(currFunc);
+                prev = currTime;
+            }else{
+                res[stack.peek()] = res[stack.peek()] + (currTime - prev + 1);
+                stack.pop();
+                prev = currTime + 1;
+            }
+        }
+        return res;
+    }
+}
