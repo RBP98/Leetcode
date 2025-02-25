70890e0244c0ee061f10fb9e176e70ee6c3cefba
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-25 16:06:12 -0500

    Time: 4 ms (99.71%), Space: 45.5 MB (40.62%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -2,26 +2,26 @@ class Solution {
     public boolean validPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
-
         while(left < right){
             if(s.charAt(left) == s.charAt(right)){
                 left++;
                 right--;
                 continue;
-            }else{
-                return checkPalindrome(s, left + 1, right) || checkPalindrome(s, left, right - 1);
             }
+
+            return checkPalindrome(s, left, right - 1) || checkPalindrome(s, left + 1, right);
         }
         return true;
     }
-    
+
     private boolean checkPalindrome(String s, int left, int right){
         while(left < right){
-            if(s.charAt(left) != s.charAt(right)) return false;
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
             left++;
             right--;
         }
         return true;
     }
-
 }
