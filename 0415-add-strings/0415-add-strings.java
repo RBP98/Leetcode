8bc7ff79f7ac4aaa04286ce1845838c5b186f7b1
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-25 18:56:58 -0500

    Time: 2 ms (63.37%), Space: 42.8 MB (44.27%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,28 +1,27 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        StringBuilder ans = new StringBuilder();
+        if(num1.length() == 0) return num2;
+        if(num2.length() == 0) return num1;
 
+        StringBuilder sb = new StringBuilder();
         int i = num1.length() - 1;
         int j = num2.length() - 1;
         int carry = 0;
+        while(i >= 0 || j >= 0){
+            int digit1 = i < 0 ? 0 : num1.charAt(i) - '0';
+            int digit2 = j < 0 ? 0 : num2.charAt(j) - '0';
 
-        while(i >= 0 || j >=0){
-            int digit1 = i >= 0 ? num1.charAt(i) - '0': 0;
-            int digit2 = j >= 0 ? num2.charAt(j) - '0': 0;
-            
-            int sum = digit1 + digit2 + carry;
-            ans.append(sum % 10);
-            carry = sum/10;
+            int value = digit1 + digit2 + carry;
+
+            carry = value/10;
+            sb.append(value % 10);
 
             i--;
             j--;
-
         }
 
-        if(carry != 0){
-            ans.append(carry);
-        }
-        
-        return ans.reverse().toString();
+        if(carry > 0) sb.append(carry);
+
+        return sb.reverse().toString();
     }
 }
