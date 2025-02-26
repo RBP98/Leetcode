55892d1585a2953959f57c8fb522f4bb82a53078
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-26 11:12:00 -0500

    Time: 48 ms (48.01%), Space: 116.9 MB (24.29%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,9 +1,9 @@
 class ListNode{
     int key;
     int value;
-    ListNode prev;
     ListNode next;
-    
+    ListNode prev;
+
     public ListNode(int key, int value){
         this.key = key;
         this.value = value;
@@ -11,31 +11,35 @@ class ListNode{
 }
 
 class LRUCache {
+    
+    Map<Integer, ListNode> map = new HashMap<>();
+    int capacity;
     ListNode head;
     ListNode tail;
-    Map<Integer, ListNode> map;
-    int capacity;
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        map = new HashMap<>();
         head = new ListNode(-1,-1);
         tail = new ListNode(-1,-1);
-
-        tail.prev = head;
         head.next = tail;
+        tail.prev = head;
     }
+
     public void add(ListNode node){
-        ListNode temp = tail.prev;
-        tail.prev = node;
-        node.next = tail;
-        node.prev = temp;
-        temp.next = node;
+        if(node != null){
+            ListNode temp = tail.prev;
+            tail.prev = node;
+            node.next = tail;
+            node.prev = temp;
+            temp.next = node;
+     
+        }
     }
+
     public void remove(ListNode node){
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
-
+    
     public int get(int key) {
         int result = -1;
         if(map.containsKey(key)){
@@ -51,20 +55,19 @@ class LRUCache {
         if(map.containsKey(key)){
             remove(map.get(key));
         }
-        ListNode node = new ListNode(key, value);
+
+        ListNode node = new ListNode(key,value);
         map.put(key, node);
         add(node);
 
-        if(map.size() > capacity){
+        if(map.size()> capacity){
             int removal = head.next.key;
             ListNode nodeToBeRemoved = map.get(removal);
+
             map.remove(removal);
             remove(nodeToBeRemoved);
         }
-        
     }
-
-
 }
 
 /**
