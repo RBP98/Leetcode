307833af58195224cedbc13c1477ff78a6aa44e8
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-25 20:43:21 -0500

    Time: 26 ms (28.19%), Space: 45.8 MB (20.76%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
@@ -1,21 +1,20 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Set<Integer> set = new HashSet<>();
+        
         Stack<Integer> stack = new Stack<>();
-      
+        Set<Integer> set = new HashSet<>();
+
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == '('){
                 stack.push(i);
-           }
-            else if(s.charAt(i) == ')'){
+            }else if(s.charAt(i) == ')'){
                 if(stack.isEmpty()){
                     set.add(i);
                 }else{
                     stack.pop();
                 }
-            }        
+            }
         }
-
         while(!stack.isEmpty()){
             set.add(stack.pop());
         }
