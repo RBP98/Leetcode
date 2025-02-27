b516e00c5fd41b6f8abc14b5f10f1bd9673c9a2d
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-02-27 15:54:22 -0500

    Time: 0 ms (100%), Space: 43.3 MB (36.34%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,35 +1,37 @@
 class Solution {
-    public void reverse(int[] nums, int start, int end){
-        while(start < end){
-            swap(nums, start, end);
-            start++;
-            end--;
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    } 
+    private void reverse(int[] nums, int i, int j){
+        while(i < j){
+            swap(nums, i, j);
+            i++;
+            j--;
         }
-        
-    }
-
-    public void swap(int[] nums, int first, int second){
-        int temp = nums[first];
-        nums[first] = nums[second];
-        nums[second] = temp;
-    }
-
-
+    } 
     public void nextPermutation(int[] nums) {
-        
-        int i = nums.length - 2;
-        while(i >= 0 && nums[i + 1] <= nums[i]){
-            i--;
-        }
-
-        if(i >= 0){
-            int j = nums.length - 1;
-            while(nums[j] <= nums[i]){
-                j--;
+        int breakpoint = -1;
+        for(int i = nums.length - 2; i >= 0; i--){
+            if(nums[i + 1] <= nums[i]){
+                continue;
+            }else{
+                breakpoint = i;
+                break;
             }
-            swap(nums, i , j);
         }
-        reverse(nums, i + 1, nums.length - 1);
-
+    
+        if(breakpoint != -1){
+            for(int i = nums.length - 1; i >= breakpoint + 1; i--){
+                if(nums[i] <= nums[breakpoint]){
+                    continue;
+                }else{
+                    swap(nums, breakpoint, i);
+                    break;
+                }
+            }
+        }
+        reverse(nums, breakpoint + 1, nums.length - 1); 
     }
 }
