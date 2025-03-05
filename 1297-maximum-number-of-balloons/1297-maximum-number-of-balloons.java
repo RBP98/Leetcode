1c3d5495195323d1b016e9b907248f6b6bda6b40
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-05 17:48:24 -0500

    Time: 2 ms (92%), Space: 41.9 MB (85.52%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+    
+        int[] first = balloonMap("balloon");
+        int[] second = balloonMap(text);
+
+        int minMod = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < 5; i++){
+            minMod = Math.min(minMod, second[i] / first[i]);
+        }
+      return minMod;
+    }
+
+    private int[] balloonMap(String text){
+        int[] res = new int[5];
+        for(char c: text.toCharArray()){
+            if(c == 'a'){
+                res[0]++;
+            }else if(c == 'b'){
+                res[1]++;
+            }else if(c == 'l'){
+                res[2]++;
+            }else if(c == 'o'){
+                res[3]++;
+            }else if(c == 'n'){
+                res[4]++;
+            }
+        }
+        
+    return res;
+    }
+}
