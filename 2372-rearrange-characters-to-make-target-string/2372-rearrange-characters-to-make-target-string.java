5dae99d3b6cb0a5e8e70f4f05ddcf12ec0c427bf
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-05 18:04:22 -0500

    Time: 0 ms (100%), Space: 41.5 MB (87.53%) - LeetHub

---
+++ 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int rearrangeCharacters(String s, String target) {
+        int[] first = freqMap(target);
+        int[] second = freqMap(s);
+
+        int minMod = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < 26; i++){
+            if(first[i] == 0){
+                continue;    
+            }
+            minMod = Math.min(minMod, second[i] / first[i]);
+        }
+      return minMod;
+    }
+
+     private int[] freqMap(String text){
+        int[] res = new int[26];
+        for(int i = 0; i < text.length(); i++){
+            char c = text.charAt(i);
+            res[c - 'a']++;
+        }    
+    return res;
+    }
+}
