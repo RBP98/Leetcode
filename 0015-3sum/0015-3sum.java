575213729c6b19aea9df1f2052cc2487b2b86db0
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-08 13:00:30 -0500

    Time: 303 ms (22.77%), Space: 53 MB (11.5%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(nums);
         Set<List<Integer>> set = new HashSet<>();
         List<List<Integer>> output = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < nums.length - 2; i++){
             int left = i + 1;
             int right = nums.length - 1;
             while(left < right){
