c3d9a95eaa74774b25a2a510ed5880491fdb1c1a
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-08 12:58:49 -0500

    Time: 305 ms (22.56%), Space: 52.9 MB (14.21%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> out = new ArrayList<>();
-        
-    
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) 
-                continue;
-            int j = i + 1;
-            int k = nums.length - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    out.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k])));
-                    j++;
-                    k--;
-                    while (j < k && nums[j] == nums[j-1])
-                        j++;
-                    while (j < k && nums[k] == nums[k + 1])
-                        k--;
+        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            int left = i + 1;
+            int right = nums.length - 1;
+            while(left < right){
+                if(nums[left] + nums[right] + nums[i] == 0){
+                    ArrayList<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[left]);
+                    temp.add(nums[right]);
+                    set.add(temp);
+                    left++;
+                    right--;
+                }else if(nums[left] + nums[right] + nums[i] < 0){
+                    left++;
+                }else{
+                    right--;
                 }
             }
-           
+            
         }
-         return out;
+
+        for(List<Integer> s: set){
+            output.add(s);
+        }
+
+
+    return output;
     }
 }
