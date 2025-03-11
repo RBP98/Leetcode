8ff8cc47c3373390f03e452726923df16a8526b9
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-11 17:03:22 -0400

    Time: 6 ms (25.62%), Space: 52 MB (24.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,53 +1,53 @@
+class Pair{
+    int first;
+    int second;
+    public Pair(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+}
 
 class Solution {
-    private void bfs(int row, int col, boolean[][] visited, char[][] grid, int m, int n){
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        boolean[][] visited = new boolean[m][n];
+        int count = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    bfs(grid, visited, i, j, m, n);
+                    count++;
+                }
+            }
+        } 
+        return count;
+    }
+
+    private void bfs(char[][] grid, boolean[][] visited, int row, int col, int m, int n){
         Queue<Pair> queue = new LinkedList<>();
         queue.add(new Pair(row, col));
+
         visited[row][col] = true;
 
         while(!queue.isEmpty()){
             Pair p = queue.poll();
             int first = p.first;
             int second = p.second;
+
             int[] drow = {0, 1, 0, -1};
-            int[] dcol = {1, 0, -1, 0};
+            int[] dcol = {1, 0 ,-1 ,0};
 
             for(int i = 0; i < 4; i++){
-                int nrow = first + drow[i];
-                int ncol = second + dcol[i];
+                int nrow = p.first + drow[i];
+                int ncol = p.second + dcol[i];
 
-                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
+                if(0 <= nrow && nrow < m && 0 <= ncol && ncol < n && grid[nrow][ncol] == '1' && !visited[nrow][ncol]){
                     queue.add(new Pair(nrow, ncol));
                     visited[nrow][ncol] = true;
-                    
-                }
-
-            }
-        }
-    }
-
-    public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int count = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(!visited[i][j] && grid[i][j] == '1'){
-                    bfs(i , j, visited, grid, m, n);
-                    count++;
                 }
             }
-        }
-    return count;
-    }
-}
-class Pair{
-    int first;
-    int second;
-
-    public Pair(int first, int second){
-        this.first = first;
-        this.second = second;
+        } 
     }
 }
