3c7d48db8a425376031e9e075de35ca48d8526fe
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-11 17:19:37 -0400

    Time: 0 ms (100%), Space: 41.4 MB (98.62%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int m = 0;
+        int n = 0;
+
+        StringBuilder sb = new StringBuilder();
+        while(m < word1.length() && n < word2.length()){
+            sb.append(word1.charAt(m));
+            sb.append(word2.charAt(n));
+            m++;
+            n++;
+        }
+        if(word1.length() > word2.length()){
+            sb.append(word1.substring(m));
+        }else{
+            sb.append(word2.substring(n));
+        }
+
+    return sb.toString();
+    }
+}
