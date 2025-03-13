3985ef83cc5eeaa54ad23a952a4e0bc5b5c1926c
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-13 14:20:27 -0400

    Time: 5 ms (99.78%), Space: 47.8 MB (69.5%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,5 +1,6 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> list = new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         for(String str: strs){
             String stringHash = createHash(str);
@@ -10,14 +11,9 @@ class Solution {
 
     private String createHash(String str){
         char arr[] = new char[26];
-        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < str.length();i++){
             arr[str.charAt(i) - 'a']++;
         }
-
-        for(int i = 0; i < 26; i++){
-            sb.append(arr[i]).append("#");
-        }
-        return sb.toString();
+        return new String(arr);
     } 
 }
