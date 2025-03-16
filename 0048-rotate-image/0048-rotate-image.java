70643370b705dd5b239efd56c01c1be934709749
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-16 02:22:58 -0400

    Time: 0 ms (100%), Space: 42.3 MB (58.31%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -3,7 +3,7 @@ class Solution {
         
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
-                if(i > j){
+                if(i < j){
                     int temp = matrix[i][j];
                     matrix[i][j] = matrix[j][i];
                     matrix[j][i] = temp;
