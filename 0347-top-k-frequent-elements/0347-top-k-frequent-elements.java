d13f00678f1303bdc23062120e84bc10a1e2ae26
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-17 18:23:48 -0400

    Time: 20 ms (6.96%), Space: 48.4 MB (72.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,39 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        List<Integer> list = new ArrayList<>();
+        int max = 0;
+        for(int i: nums){
+            map.put(i, map.getOrDefault(i, 0) + 1);
+            max = Math.max(max, map.get(i));
         }
 
-        ArrayList<Integer>[] arrayOfLists = new ArrayList[nums.length + 1];
-
-        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+        List<Integer> freqArr[] = new ArrayList[max + 1];
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
             int key = entry.getKey();
             int value = entry.getValue();
-            if(arrayOfLists[value] == null){
-                arrayOfLists[value] = new ArrayList<Integer>();
-            }
-            arrayOfLists[value].add(key);
+
+            List<Integer> temp = new ArrayList<>();
+            if(freqArr[value] != null) 
+                temp = freqArr[value];
+
+            temp.add(key); 
+            freqArr[value] = temp;
         }
-        int[] result = new int[k];
-        for(int i = arrayOfLists.length - 1; i >= 0; i--){
-            if(arrayOfLists[i] != null){
-                for(int j: arrayOfLists[i]){
-                    
+
+        for(int i = freqArr.length - 1; i >= 0; i--){
+            if(freqArr[i] != null){
+                for(int j: freqArr[i]){
                     if(k == 0){
-                        return result;
+                        break;
                     }
-                    result[k - 1] = j;
+                    list.add(j);
                     k--;
                 }
-            }
+            }   
+            
+            
         }
-        return result;
+    return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
