be4c46c1c860f57007eb4b424cbe1d9439e0fa12
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-20 13:17:51 -0400

    Time: 11 ms (95.34%), Space: 44.9 MB (85.21%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,56 +1,35 @@
 class Solution {
     public int calculate(String s) {
         int lastNumber = 0;
-        int currentNumber = 0;
-
         int result = 0;
-        char operator = '+';
+        int currNum = 0;
+        int operator = '+';
         for(int i = 0; i < s.length(); i++){
             char currChar = s.charAt(i);
 
             if(Character.isDigit(currChar)){
-                currentNumber = (currentNumber * 10) + currChar - '0';
+                currNum = (currNum * 10) + currChar - '0';
             }
 
-            if(currChar == '+' || currChar == '-' || 
+            if(currChar == '+' || currChar == '-' ||
                currChar == '*' || currChar == '/' || i == s.length() - 1){
-                //  switch (operator) {
-                //     case '+':
-                //         result += lastNumber;
-                //         lastNumber = currentNumber;
-                //         break;
-                //     case '-':
-                //         result += lastNumber;
-                //         lastNumber = -currentNumber;
-                //         break;
-                //     case '*':
-                //         lastNumber *= currentNumber;
-                //         break;
-                //     case '/':
-                //         lastNumber /= currentNumber;
-                //         break;
-                // }
 
-                if(operator == '+'){
-                    result +=lastNumber;
-                    lastNumber = currentNumber;
-                }else if(operator == '-'){
-                    result +=lastNumber;
-                    lastNumber = -currentNumber;
+                if(operator == '+' || operator == '-'){
+                    result = result + lastNumber;
+                    lastNumber = operator == '+' ? currNum : -currNum;
+
                 }else if(operator == '*'){
-                    lastNumber = lastNumber * currentNumber;
-                }else if(operator == '/'){
-                    lastNumber = lastNumber / currentNumber;
+                    lastNumber = lastNumber * currNum;
+                }else{
+                    lastNumber = lastNumber / currNum;
                 }
-
                 operator = currChar;
-                currentNumber = 0;
+                currNum = 0;
 
             }
-            
-            
         }
-        result +=lastNumber;
+
+        result = result + lastNumber;
         return result;
     }
 }
