1dbedd745501eca530c0b8eb5f012b58006af19b
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-21 03:38:03 -0400

    Time: 0 ms (100%), Space: 41.6 MB (45.88%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -2,8 +2,9 @@ class Solution {
     public int minAddToMakeValid(String s) {
         int openBrackets = 0;
         int extraBrackets = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '('){
+
+        for(char c: s.toCharArray()){
+            if(c == '('){
                 openBrackets++;
             }else{
                 if(openBrackets == 0){
