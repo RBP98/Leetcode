ed19be9d3e1abfd2b805321ff795118d856cd3b2
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-21 02:00:58 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.java
+++ 0807-custom-sort-string/0807-custom-sort-string.java
@@ -2,24 +2,26 @@ class Solution {
     public String customSortString(String order, String s) {
         int[] freqArr = new int[26];
 
-        for(int i = 0; i < s.length(); i++){
-            freqArr[s.charAt(i) - 'a']++;
+        for(char c : s.toCharArray()){
+            freqArr[c - 'a']++;
         }
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < order.length(); i++){
             int count = freqArr[order.charAt(i) - 'a'];
             for(int j = 0; j < count; j++){
                 sb.append(order.charAt(i));
-                freqArr[order.charAt(i) - 'a']--;
+            }
+            freqArr[order.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0; i < freqArr.length; i++){
+            if(freqArr[i] != 0){
+                for(int j = 0; j < freqArr[i]; j++){
+                    sb.append((char)(i + 'a'));
+                }
             }
         }
 
-        for(int i = 0; i < 26; i++){
-            while(freqArr[i] != 0){
-                sb.append((char)(i + 'a'));
-                freqArr[i]--;
-            }
-        }
         return sb.toString();
     }
 }
