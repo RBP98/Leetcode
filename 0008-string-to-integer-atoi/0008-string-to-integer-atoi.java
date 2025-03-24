2a2ff4e5b41314e695d7fc58a0adb482f75280e6
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-24 12:09:35 -0400

    Time: 1 ms (100%), Space: 42.3 MB (90.47%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,12 +1,8 @@
 class Solution {
     public int myAtoi(String s) {
         int i = 0;
-        int ans = 0;
         int sign = 1;
-        while(i < s.length() && s.charAt(i) == ' '){
-            i++;
-        }
-
+        while(i < s.length() && s.charAt(i) == ' ') i++;
         if(i < s.length() && s.charAt(i) == '+'){
             sign = 1;
             i++;
@@ -15,17 +11,16 @@ class Solution {
             i++;
         }
 
+        int num = 0;
         while(i < s.length() && Character.isDigit(s.charAt(i))){
             int value = s.charAt(i) - '0';
-            if(ans > Integer.MAX_VALUE/10 || (ans == Integer.MAX_VALUE/10 && value > Integer.MAX_VALUE%10)){
-                if(sign == 1) 
-                    return Integer.MAX_VALUE;
-                else 
-                    return Integer.MIN_VALUE;
+            if(num > Integer.MAX_VALUE/10 || (num == Integer.MAX_VALUE/10 && value > Integer.MAX_VALUE%10)){
+                return sign == 1 ? Integer.MAX_VALUE: Integer.MIN_VALUE;
             }
-            ans = ans * 10 + value;
+            num = num * 10 + value;
             i++;
         }
-    return sign == 1 ? ans: -ans;
+
+        return sign == 1 ? num : -num;
     }
 }
