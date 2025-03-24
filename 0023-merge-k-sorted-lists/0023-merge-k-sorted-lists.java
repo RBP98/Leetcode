12e7c3ed5fe0b225acd1b615c711ad3154239c1c
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-24 18:11:52 -0400

    Time: 1 ms (99.91%), Space: 44.4 MB (63.24%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,39 +10,42 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0) return null;
+        if(lists.length == 0) return null;
         int k = lists.length;
-
         while(k > 1){
-            for(int i = 0; i < k / 2; i++){
-                lists[i] = mergeTwoLists(lists[i], lists[i + (k + 1)/2]);
+            for(int i = 0; i < k/2; i++){
+                lists[i] = merge2Lists(lists[i], lists[i + (k+1)/2] );
             }
-            k = (k+1)/2;
-        }
+            k = (k + 1)/2;        
 
+        }
         return lists[0];
     }
 
-    private ListNode mergeTwoLists(ListNode list1, ListNode list2){
+    private ListNode merge2Lists(ListNode l1, ListNode l2){
         ListNode dummy = new ListNode();
         ListNode temp = dummy;
+        ListNode ptr1 = l1;
+        ListNode ptr2 = l2;
 
-        while(list1 != null && list2 != null){
-            if(list1.val < list2.val){
-                temp.next = list1;
-                temp = list1;
-                list1 = list1.next;
+        while(ptr1 != null && ptr2 != null){
+            if(ptr1.val < ptr2.val){
+                temp.next = ptr1;
+                temp = temp.next;
+                ptr1 = ptr1.next;
+                
             }else{
-                temp.next = list2;
-                temp = list2;
-                list2 = list2.next;
+                temp.next = ptr2;
+                temp = temp.next;
+                ptr2 = ptr2.next;
+                
             }
         }
 
-        if(list1 == null){
-            temp.next = list2;
-        }else if(list2 == null){
-            temp.next = list1;
+        if(ptr1 == null){
+            temp.next = ptr2;
+        }else{
+            temp.next = ptr1;
         }
 
         return dummy.next;
