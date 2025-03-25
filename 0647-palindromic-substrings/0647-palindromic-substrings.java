dabcb78587f3dbfc71cbbe467abe7897e0d596f9
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-25 02:03:00 -0400

    Time: 5 ms (96.85%), Space: 41.4 MB (92.96%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,30 +1,25 @@
 class Solution {
     public int countSubstrings(String s) {
         int result = 0;
-        for(int i = 0; i < s.length(); i++){
-            result = result + expandAroundCenter(s, i, i);
 
-            result = result + expandAroundCenter(s, i, i + 1);
+        for(int i = 0; i < s.length(); i++){
+            result = result + expand(s, i, i);
+            result = result + expand(s, i, i + 1);
         }
 
         return result;
     }
 
-    private int expandAroundCenter(String str, int low, int high){
+    private int expand(String s,int low, int high){
         int ans = 0;
-        
-        while(low >= 0 && high < str.length()){
-
-            if(str.charAt(low) != str.charAt(high)){
+        while(low >= 0 && high < s.length()){
+            if(s.charAt(low) != s.charAt(high)){
                 break;
             }
-
             low--;
             high++;
-
             ans++;
         }
-
         return ans;
     }
 }
