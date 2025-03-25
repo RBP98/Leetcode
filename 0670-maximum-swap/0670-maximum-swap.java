6f63d0a180bd3ce4f69bc69f8f2c008a653b86fe
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-03-25 15:12:04 -0400

    Time: 0 ms (100%), Space: 40.4 MB (79.68%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,22 +1,26 @@
 class Solution {
     public int maximumSwap(int num) {
-        char[] str = Integer.toString(num).toCharArray();
-        int n = str.length;
-        int[] maxFromRight = new int[n];
+        char arr[] = Integer.toString(num).toCharArray();
+        int n = arr.length;
+        int maxFromRight[] = new int[n];
         maxFromRight[n - 1] = n - 1;
         for(int i = n - 2; i >= 0; i--){
-            maxFromRight[i] = str[i] > str[maxFromRight[i + 1]] ? i : maxFromRight[i + 1];
-        }
-
-        for(int i = 0; i < n; i++){
-            if(str[maxFromRight[i]] > str[i]){
-                char temp = str[i];
-                str[i] = str[maxFromRight[i]];
-                str[maxFromRight[i]] = temp;
-                return Integer.parseInt(new String(str));
+            if(arr[i] > arr[maxFromRight[i + 1]]){
+                maxFromRight[i] = i;
+            }else{
+                maxFromRight[i] = maxFromRight[i + 1];
             }
         }
 
-    return num;
+        for(int i = 0; i < n; i++){
+            if(arr[maxFromRight[i]] > arr[i]){
+                char temp = arr[i];
+                arr[i] = arr[maxFromRight[i]];
+                arr[maxFromRight[i]] = temp;
+                return Integer.parseInt(new String(arr));
+            }
+        }
+
+        return num;
     }
 }
