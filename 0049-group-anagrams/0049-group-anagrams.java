2eb81cab5602babf855e2272a4376423abb24c15
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-02 15:08:05 -0400

    Time: 20 ms (16.39%), Space: 47.9 MB (69.4%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,23 +1,25 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> list = new ArrayList<>();
-        Map<String, List<String>> map = new HashMap<>();
-        for(String str: strs){
-            String stringHash = createHash(str);
-            map.computeIfAbsent(stringHash, e -> new ArrayList<>()).add(str);
+        // strs = ["eat","tea","tan","ate","nat","bat"]
+        Map<String, List<String>> map= new HashMap<>();
+        for(String s: strs){
+            int[] key = new int[26];
+            StringBuilder sb = new StringBuilder();
+            for(char c: s.toCharArray()){
+                key[c - 'a']++;
+            }
+            for(int i : key){
+                sb.append("#").append(i);
+            }
+            String keyStr = sb.toString();
+            List<String> tempList = new ArrayList<String>();
+            if(map.containsKey(keyStr)){
+                tempList = map.get(keyStr);
+            }
+            tempList.add(s);
+            map.put(keyStr, tempList);
+            
         }
-        return new ArrayList<>(map.values());
+    return new ArrayList<>(map.values());
     }
-
-    private String createHash(String str){
-        char arr[] = new char[26];
-        for(int i = 0; i < str.length();i++){
-            arr[str.charAt(i) - 'a']++;
-        }
-
-        // for(char i: arr){
-        //     System.out.print((i) + " ");
-        // }
-        return String.valueOf(arr);
-    } 
 }
