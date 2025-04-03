49207e44ac50869e5c327976a5693c0a426f93db
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-03 13:34:30 -0400

    Time: 3 ms (99.77%), Space: 45.1 MB (99.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,26 +1,43 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        int minVal = Integer.MAX_VALUE;
+        int maxVal = Integer.MIN_VALUE;
+        for(int i: nums){
+            minVal = Math.min(i, minVal);
+            maxVal = Math.max(i, maxVal);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
 
-        for(int i: map.keySet()){
-            pq.add(i);
-            if(k < pq.size()){
-                pq.poll();
+        int[] freqArr = new int[maxVal - minVal + 1];
+        int maxFreq = 0;
+        for(int i: nums){
+            freqArr[i - minVal]++;
+            maxFreq = Math.max(maxFreq, freqArr[i - minVal]);
+        }
+        List<Integer>[] arr = new ArrayList[maxFreq + 1];
+
+        for(int i = 0; i < freqArr.length; i++){
+            int freq = freqArr[i];
+
+            if(freq > 0){
+                if(arr[freq] == null){
+                    arr[freq] = new ArrayList<>();
+                }
+                arr[freq].add(i + minVal);
             }
-            
         }
         int[] res = new int[k];
-        for(int i = 0; i < k; i++){
-            if(!pq.isEmpty()){
-                res[i] = pq.poll();
+        int index = 0;
+        for(int i = maxFreq; i >= 0 && index < k; i--) {
+            if (arr[i] != null) {
+                for (int num : arr[i]) {
+                    res[index++] = num;
+                    if (index == k) {
+                        break;
+                    }
+                }
             }
-
         }
 
-    return res;
+        return res;
     }
 }
