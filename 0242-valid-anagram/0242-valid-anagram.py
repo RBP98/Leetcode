3b9707d480baa2730487a130d8ec5671ddf392d7
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-03 16:45:53 -0400

    Time: 27 ms (27.69%), Space: 12.7 MB (53.2%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,16 +6,18 @@ class Solution(object):
         :rtype: bool
         """
 
-        freqDict = {}
-        for letter in s:
-            freqDict[letter] = freqDict.get(letter, 0) + 1
-        for letter in t:
-            freqDict[letter] = freqDict.get(letter, 0) - 1
+        # freqDict = {}
+        # for letter in s:
+        #     freqDict[letter] = freqDict.get(letter, 0) + 1
+        # for letter in t:
+        #     freqDict[letter] = freqDict.get(letter, 0) - 1
             
-        for val in freqDict.values():
-            if val != 0:
-                return False
-        return True
+        # for val in freqDict.values():
+        #     if val != 0:
+        #         return False
+        # return True
+
+        return Counter(s) == Counter(t)
 
         
         
