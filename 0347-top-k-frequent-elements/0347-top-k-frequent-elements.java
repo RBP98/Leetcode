ff1125f4d86334b7ca0d5a69effa40f9419f95b3
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-03 12:21:01 -0400

    Time: 15 ms (40.77%), Space: 48.8 MB (43.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,39 +1,26 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        int max = 0;
-        for(int i: nums){
-            map.put(i, map.getOrDefault(i, 0) + 1);
-            max = Math.max(max, map.get(i));
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
 
-        List<Integer> freqArr[] = new ArrayList[max + 1];
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            int key = entry.getKey();
-            int value = entry.getValue();
-
-            List<Integer> temp = new ArrayList<>();
-            if(freqArr[value] != null) 
-                temp = freqArr[value];
-
-            temp.add(key); 
-            freqArr[value] = temp;
-        }
-
-        for(int i = freqArr.length - 1; i >= 0; i--){
-            if(freqArr[i] != null){
-                for(int j: freqArr[i]){
-                    if(k == 0){
-                        break;
-                    }
-                    list.add(j);
-                    k--;
-                }
-            }   
-            
+        for(int i: map.keySet()){
+            pq.add(i);
+            if(k < pq.size()){
+                pq.poll();
+            }
             
         }
-    return list.stream().mapToInt(Integer::intValue).toArray();
+        int[] res = new int[k];
+        for(int i = 0; i < k; i++){
+            if(!pq.isEmpty()){
+                res[i] = pq.poll();
+            }
+
+        }
+
+    return res;
     }
 }
