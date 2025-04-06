077b44f7d297fc2a10c32080dd0a7b0f5f8d8653
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-06 17:56:22 -0400

    Time: 0 ms (100%), Space: 40.7 MB (57.99%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,10 +1,11 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        
         return helper(n, k) + 1;
     }
-    private int helper(int n, int k){
-        if(n == 1) return 0;
-        return (helper(n - 1, k) + k) % n;
+
+    private int helper(int n, int k){ 
+        if(n == 1) return 0; 
+        int res = (helper(n - 1, k) + k) % n;
+        return res;
     }
 }
