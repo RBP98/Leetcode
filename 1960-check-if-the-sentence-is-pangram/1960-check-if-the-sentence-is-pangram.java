01beb5c615aa23e6324b77642074bdab6dbf53cd
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-15 16:45:05 -0400

    Time: 1 ms (80.26%), Space: 41.5 MB (80.56%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -3,9 +3,10 @@ class Solution {
         int[] arr = new int[26];
         int count = 26;
         for(int i = 0; i < sentence.length(); i++){
-            if(arr[(sentence.charAt(i) - 'a')] == 0) count--;
-            arr[sentence.charAt(i) - 'a']++;
-            
+            if(arr[(sentence.charAt(i) - 'a')] == 0){
+                count--;
+                arr[sentence.charAt(i) - 'a']++;
+            } 
             if(count == 0) return true;
         }
         return count == 0;
