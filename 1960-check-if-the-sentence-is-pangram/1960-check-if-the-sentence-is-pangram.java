710fbab1f9a936a104d765a8948e3cf569c8ba74
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-15 16:50:47 -0400

    Time: 0 ms (100%), Space: 41.5 MB (90.58%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        // if(sentence.length() < 26) return false;
+        if(sentence.length() < 26) return false;
         for(char c = 'a'; c <= 'z'; c++){
             if(sentence.indexOf(c) < 0){
                 return false;
