2c3bd242530974d16f08014820468b6c00e04f36
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-15 16:49:01 -0400

    Time: 0 ms (100%), Space: 41.7 MB (65.03%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,15 +1,19 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
         if(sentence.length() < 26) return false;
-        int[] arr = new int[26];
-        int count = 26;
-        for(int i = 0; i < sentence.length(); i++){
-            if(arr[(sentence.charAt(i) - 'a')] == 0){
-                count--;
-                arr[sentence.charAt(i) - 'a']++;
-            } 
-            if(count == 0) return true;
+        // int[] arr = new int[26];
+        // int count = 26;
+        for(char c = 'a'; c <= 'z'; c++){
+            if(sentence.indexOf(c) < 0){
+                return false;
+            }
+            
+            // if(arr[(sentence.charAt(i) - 'a')] == 0){
+            //     count--;
+            //     arr[sentence.charAt(i) - 'a']++;
+            // } 
+            // if(count == 0) return true;
         }
-        return count == 0;
+        return true;
     }
 }
