07fb701292f1371af7a282146925d9c92e818311
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-21 14:41:49 -0400

    Time: 3 ms (73.42%), Space: 43.7 MB (52.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        // Create sanitized string
         StringBuilder sb = new StringBuilder();
+
         for(char c: s.toCharArray()){
             if(Character.isLetterOrDigit(c)){
                 sb.append(Character.toLowerCase(c));
             }
         }
-        String sani = sb.toString();
-        
+        String sanitized = sb.toString();
+        //Init two pointers
         int left = 0;
-        int right = sani.length() - 1;
-
+        int right = sanitized.length() - 1;
+        //Run a loop until left ptr < right ptr
         while(left < right){
-            if(sani.charAt(left) != sani.charAt(right)){
-                return false;
-            }
 
+        
+        //if char At left == char At right then incr left and decr right
+            if(sanitized.charAt(left) != sanitized.charAt(right)){
+                return false;        
+            }
             left++;
             right--;
         }
-
         return true;
     }
 }
