f269fbf2faf3f830f73338bb33ecf9dffdae32c5
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-21 21:06:11 -0400

    Time: 3 ms (84.76%), Space: 49.4 MB (71.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,37 +1,4 @@
-class Pair{
-    int first;
-    int second;
-    public Pair(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
-
 class Solution {
-    private void bfs(char[][] grid, boolean[][] visited, int row, int col, int m, int n){
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(row, col));
-        visited[row][col] = true;
-
-        while(!queue.isEmpty()){
-            Pair p = queue.poll();
-            int first = p.first;
-            int second = p.second;
-
-            int[] rows = {0, -1, 0, 1};
-            int[] cols = {1, 0, -1, 0};
-
-            for(int i = 0; i < 4; i++){
-                int nrow = first + rows[i];
-                int ncol = second + cols[i];
-
-                if(nrow < m && ncol < n && nrow >= 0 && ncol >= 0 && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
-                    queue.add(new Pair(nrow, ncol));
-                    visited[nrow][ncol] = true;
-                }
-            }
-        }
-    }
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
