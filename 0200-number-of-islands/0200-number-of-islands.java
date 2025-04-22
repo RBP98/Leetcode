96e3a5dc196e982e2bf7ed2db90cd101d8fdb977
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-21 21:05:34 -0400

    Time: 4 ms (48.84%), Space: 49.6 MB (52.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -41,11 +41,19 @@ class Solution {
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(!visited[i][j] && grid[i][j] == '1'){
-                    bfs(grid, visited, i, j, m, n);
+                    dfs(grid, visited, i, j, m, n);
                     count++;
                 }
             }
         }
         return count;
+    }
+    private void dfs(char[][] grid, boolean[][] visited, int row, int col, int m, int n){
+        if(row >= m || col >= n || row < 0 || col < 0  || visited[row][col] || grid[row][col] == '0' ) return;
+        visited[row][col] = true;
+        dfs(grid, visited, row, col + 1, m, n);
+        dfs(grid, visited, row - 1, col, m, n);
+        dfs(grid, visited, row, col - 1, m, n);
+        dfs(grid, visited, row + 1, col, m, n);
     }
 }
