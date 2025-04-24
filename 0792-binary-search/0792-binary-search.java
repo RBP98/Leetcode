e2fd0b9a993965d5f947e08296a7f5c72c7e8871
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-24 16:31:57 -0400

    Time: 0 ms (100%), Space: 45.9 MB (45.72%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -4,14 +4,11 @@ class Solution {
         int high = nums.length - 1;
 
         while(low <= high){
-            int mid = high - (high - low)/2;
+            int mid = low + (high - low)/2;
             if(nums[mid] == target) return mid;
-            else if(nums[mid] < target){
-                low = mid + 1;
-            }else{
-                high = mid -1;
-            }
+            else if(nums[mid] > target) high = mid - 1;
+            else low = mid + 1;
         }
-    return -1; 
+        return -1;
     }
 }
