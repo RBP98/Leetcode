a42486d4cc3768e3ce914c0add14dab8764930aa
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-24 22:04:14 -0400

    Time: 0 ms (100%), Space: 42.9 MB (7.17%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -4,14 +4,13 @@ class Solution {
         int high = nums.length - 1;
 
         while(low < high){
-            int mid = low + (high-low)/2;
-            
+            int mid = (low + high) / 2;
+
             if(nums[mid] < nums[mid + 1]){
                 low = mid + 1;
             }else{
                 high = mid;
             }
-
         }
         return low;
     }
