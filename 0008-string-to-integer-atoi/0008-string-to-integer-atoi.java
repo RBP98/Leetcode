87c775db86dab8f03faf744351e576e6b345bd36
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-04-28 21:00:54 -0400

    Time: 1 ms (100%), Space: 42.3 MB (81.42%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,26 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
+        // if(s.length() == 0) return 0;
         int i = 0;
-        while(i < s.length() && s.charAt(i) ==  ' ') i++;
-        int sign = 1; 
-        if(i < s.length() && s.charAt(i) == '+'){
-            i++;
-        }
-        else if(i < s.length() && s.charAt(i) == '-'){
+        int sign = 1;
+        while(i < s.length() && s.charAt(i) == ' ') i++;
+        if(i < s.length() && s.charAt(i) == '-'){
             sign = -1;
             i++;
-        }
-
-        int num = 0;
-        while(i < s.length() && Character.isDigit(s.charAt(i))){
-            int digit = s.charAt(i) - '0';
-            if(num > Integer.MAX_VALUE/10 || num == Integer.MAX_VALUE/10 && digit > Integer.MAX_VALUE%10){
-                return sign < 0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;        
-            }
-            num = num * 10 + digit;
+        } 
+        else if(i < s.length() && s.charAt(i) == '+'){
             i++;
+        } 
+        int num = 0;
+        while(i < s.length()){
+            if(Character.isDigit(s.charAt(i))){
+                int digit = s.charAt(i) - '0';
+              if(num > Integer.MAX_VALUE/10 || ((num == Integer.MAX_VALUE/10) && (digit > (Integer.MAX_VALUE%10)))){ 
+               
+                    return sign == 1 ? Integer.MAX_VALUE: Integer.MIN_VALUE;
+                }
+                num = (num * 10) + digit;
+                i++;   
+            }else{
+                break;
+            }
         }
 
-        return sign < 0 ? -num : num;
+        return sign == 1 ? num : -num;
     }
 }
