23070d24a26fdf41ba6b253ba534f9496a67dd2e
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-06-20 16:15:45 -0400

    Time: 21 ms (13.06%), Space: 49.6 MB (5.68%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,25 +1,30 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // strs = ["eat","tea","tan","ate","nat","bat"]
-        Map<String, List<String>> map= new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         for(String s: strs){
-            int[] key = new int[26];
-            StringBuilder sb = new StringBuilder();
-            for(char c: s.toCharArray()){
-                key[c - 'a']++;
-            }
-            for(int i : key){
-                sb.append("#").append(i);
-            }
-            String keyStr = sb.toString();
-            List<String> tempList = new ArrayList<String>();
-            if(map.containsKey(keyStr)){
-                tempList = map.get(keyStr);
-            }
-            tempList.add(s);
-            map.put(keyStr, tempList);
             
+            int arr[] = new int[26];
+            for(int i = 0; i < s.length(); i++){
+                arr[s.charAt(i) - 'a']++; 
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < 26; i++){
+                sb.append(arr[i] - '0');
+                sb.append("#");
+            }
+
+            String key = sb.toString();
+            List<String> temp = new ArrayList<>();
+            if(map.containsKey(key)){
+                temp = map.get(key);
+            }  
+            temp.add(s); 
+            map.put(key, temp);            
+
+
         }
+
     return new ArrayList<>(map.values());
     }
 }
