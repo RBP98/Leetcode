3f4f11c2c3d8d001d6a58040fcbfba09b8feecf1
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-06-21 12:04:52 -0400

    Time: 21 ms (6.23%), Space: 48.5 MB (72.16%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,26 +1,53 @@
 class Solution {
     
     // {1: 3, 2: 5, 3: 2, 6: 4}
-
-
     // [3, ]
 
-
-
-
     public int[] topKFrequent(int[] nums, int k) {
+        
         Map<Integer, Integer> map = new HashMap<>();
-
+        int minFreq = Integer.MAX_VALUE;
+        int maxFreq = Integer.MIN_VALUE;
+        
+        // for(int i : nums){
+        //     minNum  = Math.min(minNum, i);
+        //     maxNum  = Math.max(maxNum, i);
+        // }
+        
         for(int i: nums){
             map.put(i, map.getOrDefault(i, 0) + 1);
+            minFreq = Math.min(minFreq, map.get(i));
+            maxFreq = Math.max(maxFreq, map.get(i));
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
-        for(int i : map.keySet()){
-            pq.add(i);
+
+        ArrayList<Integer>[] arr = new ArrayList[maxFreq - minFreq + 1];
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int key = entry.getKey();
+            int value = entry.getValue();
+
+            if(arr[value - minFreq] == null){
+                arr[value - minFreq] = new ArrayList<>();
+            }
+
+            arr[value - minFreq].add(key);
+
         }
+        int count = 0;
         int res[] = new int[k];
-        for(int i = 0; i < k; i++){
-            res[i] = pq.poll();
+        for(int i = arr.length - 1; i >= 0; i--){
+            if(arr[i] != null){
+                for(int num: arr[i]){
+                    if(count == k){
+                        break;
+                    }
+                    res[count] = num;
+                    count++;
+
+                    
+                }
+            }
+
         }
 
         return res;
