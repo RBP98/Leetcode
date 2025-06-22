5cc4cbb4a8430192ff91f34887761b39a929e19d
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-06-22 15:52:34 -0400

    Time: 0 ms (100%), Space: 40 MB (98.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i = 2; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        
-        
-        return dp[n];
+         int num1 = 0;
+         int num2 = 1;
+         int res = 0;
+         
+         for(int i = 0; i < n; i++){
+            res = num1 + num2;
+            num1 = num2;
+            num2 = res;
+         }
+
+         return res;
     }
 }
