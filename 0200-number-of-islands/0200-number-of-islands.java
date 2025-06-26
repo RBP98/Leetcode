d091b1d0cbaa7f48e9d0d5ad1bb67ffd32a7e1ba
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-06-26 07:46:30 -0400

    Time: 6 ms (25%), Space: 53.1 MB (5.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,54 @@
+class Pair{
+    int first;
+    int second;
+
+    public Pair(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+}
+
 class Solution {
+    private void bfs(int m, int n, int row, int col, char[][] grid, boolean[][] visited){
+        Queue<Pair> queue = new LinkedList<>();
+        
+        queue.add(new Pair(row, col));
+        visited[row][col] = true;
+
+        while(!queue.isEmpty()){
+            Pair p = queue.poll();
+            int first = p.first;
+            int second = p.second;
+            
+            int[] drow = {0, 1, 0, -1};
+            int[] dcol = {1, 0 ,-1, 0};
+
+            for(int i = 0; i < 4; i++){
+                int nrow = first + drow[i];
+                int ncol = second + dcol[i];
+
+                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && grid[nrow][ncol] == '1' && !visited[nrow][ncol]){
+                    visited[nrow][ncol] = true;
+                    queue.add(new Pair(nrow, ncol));
+                }
+            }
+        }
+    }
+
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+        boolean visited[][] = new boolean[m][n];
         int count = 0;
-        boolean[][] visited = new boolean[m][n];
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(!visited[i][j] && grid[i][j] == '1'){
-                    dfs(grid, visited, i, j, m, n);
+                    bfs(m, n, i, j, grid, visited);
                     count++;
                 }
             }
-        }
+        } 
         return count;
-    }
-    private void dfs(char[][] grid, boolean[][] visited, int row, int col, int m, int n){
-        if(row >= m || col >= n || row < 0 || col < 0  || visited[row][col] || grid[row][col] == '0' ) return;
-        visited[row][col] = true;
-        dfs(grid, visited, row, col + 1, m, n);
-        dfs(grid, visited, row - 1, col, m, n);
-        dfs(grid, visited, row, col - 1, m, n);
-        dfs(grid, visited, row + 1, col, m, n);
     }
 }
