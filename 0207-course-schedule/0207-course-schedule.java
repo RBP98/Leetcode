80f22711d164c7b5129ab0f6c541bf43158ac2b5
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-06-28 13:15:02 -0400

    Time: 7 ms (47.8%), Space: 46.3 MB (7.04%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,42 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
         
         for(int i = 0; i < numCourses; i++){
-            adj.add(new ArrayList<>());
+            list.add(new ArrayList<Integer>());
         }
-
-        for(int i = 0; i < prerequisites.length; i++){
-            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        for(int[] p : prerequisites){
+            list.get(p[1]).add(p[0]);
         }
 
         int[] indegree = new int[numCourses];
 
-        for(int i = 0; i < numCourses; i++){
-            for(int j: adj.get(i)){
+        for(int i = 0; i < list.size(); i++){
+            for(int j : list.get(i)){
                 indegree[j]++;
             }
         }
         Queue<Integer> queue = new LinkedList<>();
+
         for(int i = 0; i < numCourses; i++){
             if(indegree[i] == 0){
-                queue.add(i);             
+                queue.add(i);        
             }
         }
 
         while(!queue.isEmpty()){
             int val = queue.poll();
             numCourses--;
-            for(int j: adj.get(val)){
-                indegree[j]--;
-                if(indegree[j] == 0){
-                    queue.add(j);
+            for(int i: list.get(val)){
+                indegree[i]--;
+                if(indegree[i] == 0){
+                    queue.add(i);
                 }
             }
+
+
         }
         return numCourses == 0;
+        
     }
 }
