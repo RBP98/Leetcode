a6b56be8a0a3f058b66a159a8751aec35797bc6e
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-06-29 00:49:04 -0400

    Time: 0 ms (100%), Space: 40.7 MB (36.23%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,11 @@
 class Solution {
+    
+    int[][] dp;
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m + 1][n + 1];
+        // for(int i = 0; i <= m; i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
         for(int i = 0; i <= m; i++){
             dp[i][0] = 1;
         }
@@ -15,4 +20,19 @@ class Solution {
         }
         return dp[m - 1][n - 1];
     }
+
+    // private int helper(int i, int j){
+    //     // if(i == 0 && j == 0){
+    //     //     return 1;
+    //     // }
+
+    //     if(i < 0 || j < 0){
+    //         return 0;
+    //     }
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
+    //     int left = helper(i, j - 1);
+    //     int up = helper(i - 1, j);
+    //     return dp[i][j] = left + up;
+    // }
 }
