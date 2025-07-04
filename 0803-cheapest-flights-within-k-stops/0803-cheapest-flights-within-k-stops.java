4474c57fcab5aabc931407912fa3430081f47e25
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-07-04 19:58:03 -0400

    Time: 8 ms (33.23%), Space: 44.3 MB (84.13%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -1,22 +1,38 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        // Distance from source to all other nodes.
+        Map<Integer, List<int[]>> adj = new HashMap<>();
+        for (int[] i : flights)
+            adj.computeIfAbsent(i[0], value -> new ArrayList<>()).add(new int[] { i[1], i[2] });
+
         int[] dist = new int[n];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src] = 0;
 
-        // Run only K+1 times since we want shortest distance in K hops
-        for (int i = 0; i <= k; i++) {
-            // Create a copy of dist vector.
-            int[] temp = Arrays.copyOf(dist, n);
-            for (int[] flight : flights) {
-                if (dist[flight[0]] != Integer.MAX_VALUE) {
-                    temp[flight[1]] = Math.min(temp[flight[1]], dist[flight[0]] + flight[2]);
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] { src, 0 });
+        int stops = 0;
+
+        while (stops <= k && !q.isEmpty()) {
+            int sz = q.size();
+            // Iterate on current level.
+            while (sz-- > 0) {
+                int[] temp = q.poll();
+                int node = temp[0];
+                int distance = temp[1];
+
+                if (!adj.containsKey(node))
+                    continue;
+                // Loop over neighbors of popped node.
+                for (int[] e : adj.get(node)) {
+                    int neighbour = e[0];
+                    int price = e[1];
+                    if (price + distance >= dist[neighbour])
+                        continue;
+                    dist[neighbour] = price + distance;
+                    q.offer(new int[] { neighbour, dist[neighbour] });
                 }
             }
-            // Copy the temp vector into dist.
-            dist = temp;
+            stops++;
         }
         return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
     }
-};
+}
