9f7044d13b4a53bd70706a995eb84d7c7b25b865
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-07-05 16:08:51 -0400

    Time: 62 ms (41.33%), Space: 62.1 MB (8.14%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,41 +1,13 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        List<Integer> list = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i: nums){
-            list.add(i);
-        }
-
-        return quickSelect(list, k);
-
-    }
-
-    private int quickSelect(List<Integer> list, int k){
-        Random rand = new Random();
-        int pivotIndex = rand.nextInt(list.size());
-        int pivot = list.get(pivotIndex);
-
-        List<Integer> left = new ArrayList<>();
-        List<Integer> mid = new ArrayList<>();
-        List<Integer> right = new ArrayList<>();
-
-        for(int i: list){
-            if(i == pivot){
-                mid.add(i);
-            }else if(i > pivot){
-                left.add(i);
-            }else{
-                right.add(i);
+            pq.add(i);
+            if(pq.size() > k){
+                pq.poll();
             }
-        }
-
-        if(left.size() >= k){
-            return quickSelect(left, k);
-        }
-
-        if(left.size() + mid.size() < k){
-            return quickSelect(right, k - (left.size() + mid.size()));
-        }
-
-        return pivot;
+        } 
+        
+        return pq.peek();
     }
 }
