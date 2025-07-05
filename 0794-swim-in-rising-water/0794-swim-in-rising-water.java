9c28d5e85750f37ff7de4d2f22f914606249356c
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-07-05 15:02:52 -0400

    Time: 12 ms (48.26%), Space: 44.6 MB (50.92%) - LeetHub

---
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -0,0 +1,45 @@
+class Cell{
+    int row, col, time;
+    public Cell(int row, int col, int time){
+        this.row = row;
+        this.col = col;
+        this.time = time;
+    }
+}
+
+class Solution {
+    public int swimInWater(int[][] grid) {
+        int n = grid.length;
+        PriorityQueue<Cell> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.time, b.time));
+        boolean[][] visited = new boolean[n][n];
+
+        pq.add(new Cell(0, 0, grid[0][0]));
+
+
+        int[] drow = {0, 1, 0, -1};
+        int[] dcol = {1, 0, -1, 0};
+
+        while(!pq.isEmpty()){
+            Cell c = pq.poll();
+
+            int row = c.row;
+            int col = c.col;
+            int time = c.time;
+
+            if (visited[row][col]) continue;
+            visited[row][col] = true;
+
+            if(row == n-1 && col == n-1) return time; 
+
+            for(int i = 0; i < 4; i++){
+                int nrow = drow[i] + row;
+                int ncol = dcol[i] + col;
+                
+                if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < n && !visited[nrow][ncol]){
+                    pq.add(new Cell(nrow, ncol, Math.max(time, grid[nrow][ncol])));
+                }
+            }
+        }
+        return -1;
+    }
+}
