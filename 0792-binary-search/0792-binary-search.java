d83f97c75be836aec6c4e59cf3bea6fcd4ba794f
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-08-21 03:37:33 -0400

    Time: 0 ms (100%), Space: 45.7 MB (78.37%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,15 +1,19 @@
 class Solution {
+    public boolean isBefore(int num, int target){
+        return num < target;
+    }
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+        int n = nums.length;
+        int l = 0;
+        int r = n - 1;
+        if(n == 0 || isBefore(nums[r], target)) return -1;
+        if(nums[l] == target) return l;
 
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
+        while(r - l > 1){
+            int mid = l + (r - l)/2;
+            if(isBefore(nums[mid], target)) l = mid;
+            else r = mid; 
         }
-
-        return -1;
+        return nums[r] == target ? r : -1;
     }
 }
