9b4b6a6a48bdb498a1cc07e31b9e30f440bc58bc
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-12-30 19:58:01 -0500

    Time: 34 ms (56.67%), Space: 59.1 MB (59.29%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,10 +3,11 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> list = new ArrayList<>();
         for(int i = 0; i < nums.length - 2; i++){
+            
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
             int j = i + 1;
             int k = nums.length - 1;
 
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
 
             while(j < k){
                 if(nums[i] + nums[j] + nums[k] == 0){
