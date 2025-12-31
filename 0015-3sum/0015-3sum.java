5982f1b8579c22d62eacb528c37363484da59a14
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-12-30 19:56:57 -0500

    Time: 33 ms (68.39%), Space: 58.7 MB (92.56%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,36 +3,36 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> list = new ArrayList<>();
         for(int i = 0; i < nums.length - 2; i++){
+            int j = i + 1;
+            int k = nums.length - 1;
 
-            if(i > 0 && nums[i - 1] == nums[i]) continue;
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int left = i + 1;
-            int right = nums.length - 1;
-            while(left < right){
-                if(nums[i] + nums[left] + nums[right] == 0){
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[left]);
-                    temp.add(nums[right]);
-                    list.add(temp);
-                    while(left < right && nums[left] == nums[left + 1])
-                        left++;
-                    while(left < right && nums[right] == nums[right - 1])
-                        right--;
+            while(j < k){
+                if(nums[i] + nums[j] + nums[k] == 0){
+                    list.add(List.of(nums[i], nums[j], nums[k]));
+                    
+                    while(j < k && nums[j] == nums[j + 1]){
+                        j++;
+                    }
+                    
+                    while(j < k && nums[k] == nums[k - 1]){
+                        k--;
+                    }
+                    
+                    j++;
+                    k--;
 
-                    left++;
-                    right--;
-                }else if(nums[i] + nums[left] + nums[right] < 0){
-                        left++;  
-                        
-                }else{
-                        right--;
 
                 }
+                else if(nums[i] + nums[j] + nums[k] < 0){
+                    j++;
+                }
+                else{
+                    k--;
+                }
             }
-            
         }
-
         return list;
     }
 }
