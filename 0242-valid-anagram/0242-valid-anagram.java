9758ee44bedb17f34fedbf3c436d97b9af675fc9
RBP98 <47711591+RBP98@users.noreply.github.com>
2025-12-31 11:00:28 -0500

    Time: 2 ms (99.5%), Space: 44.7 MB (56.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
+        // if(s.length() != t.length()) return false;
         char[] sArr = new char[26];
 
         for(char c: s.toCharArray()){
