264c0e4379a3b183b49668e1e39d13e203b058f5
RBP98 <47711591+RBP98@users.noreply.github.com>
2026-01-09 21:20:15 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,18 +1,16 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int count = 0;
         Map<Integer, Integer> map = new HashMap<>();
-        int prefix = 0;
         map.put(0, 1);
+        int prefix = 0;
+        int count = 0;
         for(int i = 0; i < nums.length; i++){
             prefix = prefix + nums[i];
-
             if(map.containsKey(prefix - k)){
-                count  = count + map.get(prefix - k);
+                count++;
             }
             map.put(prefix, map.getOrDefault(prefix, 0) + 1);
         }
-
         return count;
     }
 }
