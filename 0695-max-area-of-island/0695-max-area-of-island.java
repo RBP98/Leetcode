df7635bc61179cc5886b78ad2d17b33deb48cd0d
RBP98 <47711591+RBP98@users.noreply.github.com>
2026-02-21 16:31:15 -0500

    Time: 4 ms (11.87%), Space: 47 MB (15.98%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,58 +1,49 @@
-//Same as number of islands. Except counting the islands, here we count the size of each island in the bfs and compare it with maxArea while updating maxArea each time area of current island is greater than maxArea
-
-
-class Pair{
-    int first;
-    int second;
-    
-    public Pair(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
 class Solution {
-
-    private int bfs(int row, int col, boolean[][] visited,int[][] grid, int m, int n){
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(row, col));
-        visited[row][col] = true;
-        int area = 1;
+    public int bfs(int r, int c, int rows, int cols, boolean[][] visited, int[][] grid){
+        Queue<int[]> queue = new LinkedList<>();
+        visited[r][c] = true;
+        queue.add(new int[]{r, c});
+        int count = 1;
         while(!queue.isEmpty()){
-            Pair p = queue.poll();
-            int first = p.first;
-            int second = p.second;
+            int[] temp = queue.poll();
+            int first = temp[0];
+            int second = temp[1];
 
             int[] drow = {0, 1, 0, -1};
             int[] dcol = {1, 0, -1, 0};
-
+            
             for(int i = 0; i < 4; i++){
                 int nrow = first + drow[i];
                 int ncol = second + dcol[i];
 
-                if(nrow >= 0 && nrow < m && ncol >=0 && ncol < n && !visited[nrow][ncol] && grid[nrow][ncol] == 1){
+                if(nrow >= 0 && nrow < rows && ncol >= 0 && ncol < cols && grid[nrow][ncol] == 1 && !visited[nrow][ncol]){
+                    queue.add(new int[]{nrow, ncol});
                     visited[nrow][ncol] = true;
-                    queue.add(new Pair(nrow, ncol));
-                    area++;
+                    count++;
                 }
             }
+
+
+
         }
-        return area;
+        return count;
+
     }
-    public int maxAreaOfIsland(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int maxArea = 0;
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1 && !visited[i][j]){
-                    int currentArea = bfs(i, j, visited, grid, m, n); 
-                    maxArea = Math.max(maxArea, currentArea);
+    public int maxAreaOfIsland(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int maxArea = 0;
+        boolean visited[][] = new boolean[rows][cols];
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(!visited[i][j] && grid[i][j] == 1){
+                    maxArea = Math.max(maxArea, bfs(i, j, rows, cols, visited, grid));
                 }
             }
         }
 
-    return maxArea;
+        return maxArea;
     }
 }
