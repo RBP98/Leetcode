734f449265c4a36837d088053a8194b8dbb49d55
RBP98 <47711591+RBP98@users.noreply.github.com>
2026-02-21 15:35:10 -0500

    Time: 1 ms (99.68%), Space: 45.4 MB (65.2%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,26 +1,27 @@
 class Solution {
     public int compress(char[] chars) {
-        int i = 0;
-        int index = 0;
 
-        while(i < chars.length){
-            char curr = chars[i];
+        int writeIndex = 0;
+        for(int i = 0; i < chars.length;){
+            char currChar = chars[i];
             int count = 0;
-            while(i < chars.length && curr == chars[i]){
+            while(i < chars.length && currChar == chars[i]){
                 count++;
                 i++;
             }
-            chars[index] = curr;
-            index++;
 
+            chars[writeIndex++] = currChar;
             if(count > 1){
                 String s = String.valueOf(count);
                 for(int j = 0; j < s.length(); j++){
-                    chars[index] = s.charAt(j);
-                    index++;
+                    chars[writeIndex++] = s.charAt(j);
                 }
             }
+            
+
+
         }
-        return index;
+
+        return writeIndex;
     }
 }
