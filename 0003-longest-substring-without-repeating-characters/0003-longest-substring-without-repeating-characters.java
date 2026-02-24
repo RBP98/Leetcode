467a71d3b6e0b86c78e726e40a03d8839a709e96
RBP98 <47711591+RBP98@users.noreply.github.com>
2026-02-23 21:44:43 -0500

    Time: 7 ms (36.87%), Space: 46.2 MB (58.05%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -3,18 +3,13 @@ class Solution {
         int left = 0;
         int right = 0;
         Set<Character> set = new HashSet<>();
+        
         int longest = 0;
-        //abcabcbb          s.length() = 8 
-        //left = 3
-        //right = 6
-        //set {}
-        //longest = 3
         while(right < s.length()){
             if(!set.contains(s.charAt(right))){
                 set.add(s.charAt(right));
                 longest = Math.max(longest, right - left + 1);
                 right++;
-            
             }else{
                 while(right < s.length() && set.contains(s.charAt(right))){
                     set.remove(s.charAt(left));
@@ -22,6 +17,7 @@ class Solution {
                 }
             }
         }
+
         return longest;
     }
 }
